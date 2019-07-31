b50b2fa9f1b9cea3d27a6d18592f59b909f0a676
Alex Devero <deveroalex@gmail.com>
2019-07-31 10:30:27 +0200

    Add typography styles

---
+++ src/components/typography.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+import { Typography } from './constants'
+
+export const HeadingH1 = styled.h1`
+  font-size: ${Typography.headingH1};
+  font-weight: 700;
+`
+
+export const HeadingH2 = styled.h2`
+  font-size: ${Typography.headingH2};
+  font-weight: 700;
+`
+
+export const HeadingH3 = styled.h3`
+  font-size: ${Typography.headingH3};
+  font-weight: 700;
+`
+
+export const HeadingH4 = styled.h4`
+  font-size: ${Typography.headingH4};
+  font-weight: 700;
+`
+
+export const HeadingH5 = styled.h5`
+  font-size: ${Typography.headingH5};
+  font-weight: 700;
+`
+
+export const HeadingH6 = styled.h6`
+  font-size: ${Typography.headingH6};
+  font-weight: 700;
+`
+
+export const TextBody = styled.p`
+  font-size: ${Typography.body};
+`
+
+export const TextSmall = styled.p`
+  font-size: ${Typography.small};
+`
+
+export const TextSmaller = styled.p`
+  font-size: ${Typography.smaller};
+`
