5026cc7361fa1abab50a9b9821bd7788e43c7db0
Alex Devero <deveroalex@gmail.com>
2019-07-31 10:17:42 +0200

    Add mock data for dashboard graphs

---
+++ src/data/data-pr-body-weight.ts
@@ -0,0 +1,46 @@
+export const dataPrBodyWeight = [
+  {
+    key: 'Muscle-up',
+    data: 25
+  },
+  {
+    key: 'Chin-up',
+    data: 44
+  },
+  {
+    key: 'Pull-up',
+    data: 39
+  },
+  {
+    key: 'Dips',
+    data: 45
+  },
+  {
+    key: 'Korean dips',
+    data: 35
+  },
+  {
+    key: 'Push-up',
+    data: 35
+  },
+  {
+    key: 'Squat',
+    data: 55
+  },
+  {
+    key: 'Pistol squat',
+    data: 23
+  },
+  {
+    key: 'Knee raises',
+    data: 68
+  },
+  {
+    key: 'Dragon flag',
+    data: 72
+  },
+  {
+    key: 'Box jumps',
+    data: 23
+  }
+]
---
+++ src/data/data-pr-equipment.ts
@@ -0,0 +1,38 @@
+export const dataPrEquipment = [
+  {
+    key: 'Barbell curl',
+    data: 35
+  },
+  {
+    key: 'Dumbbell curl',
+    data: 24
+  },
+  {
+    key: 'Hammer curl',
+    data: 22
+  },
+  {
+    key: 'Concentration curl',
+    data: 20
+  },
+  {
+    key: 'Dumbbell flies',
+    data: 18
+  },
+  {
+    key: 'Deadlift',
+    data: 230
+  },
+  {
+    key: 'Bench press',
+    data: 120
+  },
+  {
+    key: 'Triceps extension',
+    data: 22
+  },
+  {
+    key: 'Triceps Pushdown',
+    data: 32
+  }
+]
