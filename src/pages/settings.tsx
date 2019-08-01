c58ceff73d6295bd35c16fc2023628df4b1b7a29
Alex Devero <deveroalex@gmail.com>
2019-08-01 10:24:13 +0200

    Create settings screen

---
+++ src/pages/settings.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+import { View } from './../components/constants'
+
+const Settings = () => {
+  return (
+    <View>
+      This is Settings page{' '}
+      <span aria-label="Settings" aria-hidden={true}>
+        🛠
+      </span>
+    </View>
+  )
+}
+
+export default Settings
