6b27b8def2b64ea51b248a6e93f16c6c62d3e1be
Alex Devero <deveroalex@gmail.com>
2019-08-02 10:32:01 +0200

    Add settings store

---
+++ src/stores/store-settings.ts
@@ -0,0 +1,43 @@
+// TODO: Implement firebase and get data from dtb
+
+interface SettingsStoreInterface {
+  firstName: string;
+  lastName: string;
+  email: string;
+
+  username: string;
+  sex: string;
+  yearsPropTraining: number;
+
+  height: number;
+  weight: number;
+  bodyFat: number;
+
+  appTheme: boolean;// 'light' | 'dark';
+  units: boolean;//'metric' | 'imperial';
+
+  weeklyProgressReport: boolean;
+  occasionalTips: boolean;
+  productAnnouncements: boolean;
+}
+
+export const SettingsStore: SettingsStoreInterface = {
+  firstName: 'Tom',
+  lastName: 'Jones',
+  email: 'tomjones@email.com',
+
+  username: 'tomjones',
+  sex: 'male',
+  yearsPropTraining: 1,
+
+  height: 187,
+  weight: 65,
+  bodyFat: 8.5,
+
+  appTheme: false,// 'light',
+  units: true,// 'metric',
+
+  weeklyProgressReport: true,
+  occasionalTips: true,
+  productAnnouncements: true
+}
