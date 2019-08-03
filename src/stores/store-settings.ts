42f4001bdb60722b726ee0b98a342886f5021121
Alex Devero <deveroalex@gmail.com>
2019-08-03 12:05:51 +0200

    Add birth date to settings store

--- src/stores/store-settings.ts
+++ src/stores/store-settings.ts
@@ -4,7 +4,7 @@ interface SettingsStoreInterface {
   firstName: string;
   lastName: string;
   email: string;
-  date: string;
+  birthDate: string;
 
   username: string;
   sex: string;
@@ -26,7 +26,7 @@ export const SettingsStore: SettingsStoreInterface = {
   firstName: 'Tom',
   lastName: 'Jones',
   email: 'tomjones@email.com',
-  date: '2019-08-07',
+  birthDate: '2019-08-07',
 
   username: 'tomjones',
   sex: 'male',
