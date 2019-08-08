a19108a70b131e9ae149f074cf3a354c6afba79f
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:54:08 +0200

    Add library of lower back exercises

---
+++ src/stores/store-exercises-lower-back.ts
@@ -0,0 +1,160 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesLowerBackStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Other',
+    id: 'lower_back_000',
+    muscle: 'Lower Back',
+    name: 'Atlas Stones'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_001',
+    muscle: 'Lower Back',
+    name: 'Deficit Deadlift'
+  }, {
+    equipment: 'Other',
+    id: 'lower_back_002',
+    muscle: 'Lower Back',
+    name: 'Axle Deadlift'
+  }, {
+    equipment: 'Other',
+    id: 'lower_back_003',
+    muscle: 'Lower Back',
+    name: 'Hyperextensions (Back Extensions)'
+  }, {
+    equipment: 'Body Only',
+    id: 'lower_back_004',
+    muscle: 'Lower Back',
+    name: 'Hyperextensions With No Hyperextension Bench'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_005',
+    muscle: 'Lower Back',
+    name: 'Deadlift with Bands'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_006',
+    muscle: 'Lower Back',
+    name: 'Deadlift with Chains'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_007',
+    muscle: 'Lower Back',
+    name: 'Rack Pull with Bands'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_008',
+    muscle: 'Lower Back',
+    name: 'Rack Pulls'
+  }, {
+    equipment: 'Body Only',
+    id: 'lower_back_009',
+    muscle: 'Lower Back',
+    name: 'Superman'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'lower_back_010',
+    muscle: 'Lower Back',
+    name: 'Weighted Ball Hyperextension'
+  }, {
+    equipment: 'Machine',
+    id: 'lower_back_011',
+    muscle: 'Lower Back',
+    name: 'Seated Back Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_012',
+    muscle: 'Lower Back',
+    name: 'Stiff Leg Barbell Good Morning'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_013',
+    muscle: 'Lower Back',
+    name: 'Seated Good Mornings'
+  }, {
+    equipment: 'Other',
+    id: 'lower_back_014',
+    muscle: 'Lower Back',
+    name: 'Atlas Stone Trainer'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'lower_back_015',
+    muscle: 'Lower Back',
+    name: 'Hug A Ball'
+  }, {
+    equipment: 'None',
+    id: 'lower_back_016',
+    muscle: 'Lower Back',
+    name: 'Dancer\'s Stretch'
+  }, {
+    equipment: 'None',
+    id: 'lower_back_017',
+    muscle: 'Lower Back',
+    name: 'Child\'s Pose'
+  }, {
+    equipment: 'None',
+    id: 'lower_back_018',
+    muscle: 'Lower Back',
+    name: 'Pelvic Tilt Into Bridge'
+  }, {
+    equipment: 'None',
+    id: 'lower_back_019',
+    muscle: 'Lower Back',
+    name: 'Cat Stretch'
+  }, {
+    equipment: 'Foam Roll',
+    id: 'lower_back_020',
+    muscle: 'Lower Back',
+    name: 'Lower Back-SMR'
+  }, {
+    equipment: 'None',
+    id: 'lower_back_021',
+    muscle: 'Lower Back',
+    name: 'Hug Knees To Chest'
+  }, {
+    equipment: 'None',
+    id: 'lower_back_022',
+    muscle: 'Lower Back',
+    name: 'Crossover Reverse Lunge'
+  }, {
+    equipment: 'None',
+    id: 'lower_back_023',
+    muscle: 'Lower Back',
+    name: 'Standing Pelvic Tilt'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'lower_back_024',
+    muscle: 'Lower Back',
+    name: 'Pyramid'
+  }, {
+    equipment: 'Other',
+    id: 'lower_back_025',
+    muscle: 'Lower Back',
+    name: 'Keg Load'
+  }, {
+    equipment: 'Barbell',
+    id: 'lower_back_026',
+    muscle: 'Lower Back',
+    name: 'Reverse Band Deadlift'
+  }, {
+    equipment: 'Body Only',
+    id: 'lower_back_027',
+    muscle: 'Lower Back',
+    name: 'Partner Supermans With Alternating High-Five'
+  }, {
+    equipment: 'Other',
+    id: 'lower_back_028',
+    muscle: 'Lower Back',
+    name: 'Partner Flat-Bench Back Extension'
+  }, {
+    equipment: 'Other',
+    id: 'lower_back_029',
+    muscle: 'Lower Back',
+    name: 'Partner Flat-Bench Back Extension With Hold'
+  }
+]
