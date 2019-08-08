347d7cd730f1c0445588e07c9df5d1653a7a5b0c
Alex Devero <deveroalex@gmail.com>
2019-08-08 17:09:01 +0200

    Import exercises filter & table

--- src/pages/exercises.tsx
+++ src/pages/exercises.tsx
@@ -2,40 +2,13 @@ import * as React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
 
-import { GridCol, GridRow } from './../components/grid'
 import { HeadingH5 } from './../components/typography'
-import { Fieldset, Checkbox } from './../components/form-elements'
 import { View } from './../components/constants'
 
-import { ExercisesStore } from './../stores/store-exercises'
+import { ExercisesFilter } from './../pages/exercises/exercises-filter'
+import { ExercisesTable } from './../pages/exercises/exercises-table'
 
-const ExercisesFilterRow = styled(GridRow)`
-  margin-top: 16px;
-
-  @media (max-width: 575px) {
-    & > div + div {
-      margin-top: 16px;
-    }
-  }
-
-  @media (max-width: 767px) {
-    & > div:nth-of-type(n+3) {
-      margin-top: 16px;
-    }
-  }
-
-  @media (max-width: 991px) {
-    & > div:nth-of-type(n+4) {
-      margin-top: 16px;
-    }
-  }
-
-  @media (min-width: 992px) {
-    & > div:nth-of-type(n+5) {
-      margin-top: 16px;
-    }
-  }
-`
+import { ExercisesFilterStore } from '../stores/store-exercises-filter'
 
 const FilterButton = styled.button`
   padding: 0;
@@ -58,35 +31,43 @@ const FilterButton = styled.button`
 const Exercises = () => {
   const [filterOpen, setFilterOpen] = React.useState(false)
   const [filterOptions, setFilterOptions] = React.useState({
-    showAll: ExercisesStore.showAll,
-    showAbs: ExercisesStore.showAbs,
-    showBack: ExercisesStore.showBack,
-    showBiceps: ExercisesStore.showBiceps,
-    showCardio: ExercisesStore.showCardio,
-    showChest: ExercisesStore.showChest,
-    showForearm: ExercisesStore.showForearm,
-    showGlutes: ExercisesStore.showGlutes,
-    showLowerLegs: ExercisesStore.showLowerLegs,
-    showShoulders: ExercisesStore.showShoulders,
-    showTriceps: ExercisesStore.showTriceps,
-    showUpperLegs: ExercisesStore.showUpperLegs
+    showAbs: ExercisesFilterStore.showAbs,
+    showAll: ExercisesFilterStore.showAll,
+    showBiceps: ExercisesFilterStore.showBiceps,
+    showCalves: ExercisesFilterStore.showCalves,
+    showCardio: ExercisesFilterStore.showCardio,
+    showForearms: ExercisesFilterStore.showForearms,
+    showGlutes: ExercisesFilterStore.showGlutes,
+    showChest: ExercisesFilterStore.showChest,
+    showLats: ExercisesFilterStore.showLats,
+    showLowerBack: ExercisesFilterStore.showLowerBack,
+    showMiddleBack: ExercisesFilterStore.showMiddleBack,
+    showNeck: ExercisesFilterStore.showNeck,
+    showQuadriceps: ExercisesFilterStore.showQuadriceps,
+    showShoulders: ExercisesFilterStore.showShoulders,
+    showTraps: ExercisesFilterStore.showTraps,
+    showTriceps: ExercisesFilterStore.showTriceps
   })
 
   const handleFilterChange = (filter: string) => {
     if (filter === 'showAll') {
       setFilterOptions({
-        showAll: true,
         showAbs: false,
-        showBack: false,
+        showAll: true,
         showBiceps: false,
+        showCalves: false,
         showCardio: false,
-        showChest: false,
-        showForearm: false,
+        showForearms: false,
         showGlutes: false,
-        showLowerLegs: false,
+        showChest: false,
+        showLats: false,
+        showLowerBack: false,
+        showMiddleBack: false,
+        showNeck: false,
+        showQuadriceps: false,
         showShoulders: false,
-        showTriceps: false,
-        showUpperLegs: false
+        showTraps: false,
+        showTriceps: false
       })
     } else {
       const filterOptionStateIndex = Object.keys(filterOptions).findIndex((key) => key === filter)
@@ -119,139 +100,9 @@ const Exercises = () => {
           <FilterButton onClick={() => setFilterOpen(!filterOpen)}><span className="fas fa-filter" title="Open filter" /></FilterButton>
         </div>
 
-        {filterOpen && <ExercisesFilterRow>
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showAll}
-                id="app-theme"
-                label="Show all"
-                onChange={() => handleFilterChange('showAll')}
-              />
-            </Fieldset>
-          </GridCol>
+        {filterOpen && <ExercisesFilter filterOptions={filterOptions} handleFilterChange={handleFilterChange} />}
 
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showBiceps}
-                id="app-theme"
-                label="Show biceps"
-                onChange={() => handleFilterChange('showBiceps')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showTriceps}
-                id="app-theme"
-                label="Show triceps"
-                onChange={() => handleFilterChange('showTriceps')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showAbs}
-                id="app-theme"
-                label="Show abs"
-                onChange={() => handleFilterChange('showAbs')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showForearm}
-                id="app-theme"
-                label="Show forearms"
-                onChange={() => handleFilterChange('showForearm')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showShoulders}
-                id="app-theme"
-                label="Show shoulders"
-                onChange={() => handleFilterChange('showShoulders')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showBack}
-                id="app-theme"
-                label="Show back"
-                onChange={() => handleFilterChange('showBack')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showChest}
-                id="app-theme"
-                label="Show chest"
-                onChange={() => handleFilterChange('showChest')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showGlutes}
-                id="app-theme"
-                label="Show glutes"
-                onChange={() => handleFilterChange('showGlutes')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showUpperLegs}
-                id="app-theme"
-                label="Show upper legs"
-                onChange={() => handleFilterChange('showUpperLegs')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showLowerLegs}
-                id="app-theme"
-                label="Show lower legs"
-                onChange={() => handleFilterChange('showLowerLegs')}
-              />
-            </Fieldset>
-          </GridCol>
-
-          <GridCol sm={6} md={4} lg={3}>
-            <Fieldset>
-              <Checkbox
-                checked={filterOptions.showCardio}
-                id="app-theme"
-                label="Show cardio"
-                onChange={() => handleFilterChange('showCardio')}
-              />
-            </Fieldset>
-          </GridCol>
-        </ExercisesFilterRow>}
+        <ExercisesTable />
       </View>
     </>
   )
