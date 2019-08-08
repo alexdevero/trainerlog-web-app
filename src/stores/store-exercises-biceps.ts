d1e90a5f83da49d34ff0e6e1b212390ab86eef38
Alex Devero <deveroalex@gmail.com>
2019-08-08 15:17:28 +0200

    Add library of biceps exercises

---
+++ src/stores/store-exercises-biceps.ts
@@ -0,0 +1,300 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesBicepsStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Dumbbell',
+    id: 'biceps_000',
+    muscle: 'Biceps',
+    name: 'Incline Hammer Curls'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_001',
+    muscle: 'Biceps',
+    name: 'Wide-Grip Standing Barbell Curl'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'biceps_002',
+    muscle: 'Biceps',
+    name: 'Spider Curl'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'biceps_003',
+    muscle: 'Biceps',
+    name: 'EZ-Bar Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_004',
+    muscle: 'Biceps',
+    name: 'Hammer Curls'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_005',
+    muscle: 'Biceps',
+    name: 'Zottman Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_006',
+    muscle: 'Biceps',
+    name: 'Biceps Curl To Shoulder Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_007',
+    muscle: 'Biceps',
+    name: 'Concentration Curls'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_008',
+    muscle: 'Biceps',
+    name: 'Barbell Curl'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_009',
+    muscle: 'Biceps',
+    name: 'Overhead Cable Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_010',
+    muscle: 'Biceps',
+    name: 'Flexor Incline Dumbbell Curls'
+  }, {
+    equipment: 'Machine',
+    id: 'biceps_011',
+    muscle: 'Biceps',
+    name: 'Machine Bicep Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_012',
+    muscle: 'Biceps',
+    name: 'Dumbbell Bicep Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_013',
+    muscle: 'Biceps',
+    name: 'Cross Body Hammer Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_014',
+    muscle: 'Biceps',
+    name: 'Close-Grip EZ Bar Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_015',
+    muscle: 'Biceps',
+    name: 'Barbell Curls Lying Against An Incline'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'biceps_016',
+    muscle: 'Biceps',
+    name: 'Reverse Barbell Preacher Curls'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_017',
+    muscle: 'Biceps',
+    name: 'Preacher Curl'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_018',
+    muscle: 'Biceps',
+    name: 'Standing One-Arm Cable Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_019',
+    muscle: 'Biceps',
+    name: 'Seated Close-Grip Concentration Barbell Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_020',
+    muscle: 'Biceps',
+    name: 'Alternate Incline Dumbbell Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_021',
+    muscle: 'Biceps',
+    name: 'Dumbbell Alternate Bicep Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_022',
+    muscle: 'Biceps',
+    name: 'One Arm Dumbbell Preacher Curl'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_023',
+    muscle: 'Biceps',
+    name: 'Reverse Cable Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_024',
+    muscle: 'Biceps',
+    name: 'Standing Concentration Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_025',
+    muscle: 'Biceps',
+    name: 'Incline Dumbbell Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_026',
+    muscle: 'Biceps',
+    name: 'Close-Grip Standing Barbell Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_027',
+    muscle: 'Biceps',
+    name: 'Preacher Hammer Dumbbell Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_028',
+    muscle: 'Biceps',
+    name: 'Alternate Hammer Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_029',
+    muscle: 'Biceps',
+    name: 'Seated Dumbbell Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_030',
+    muscle: 'Biceps',
+    name: 'Standing One-Arm Dumbbell Curl Over Incline Bench'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_031',
+    muscle: 'Biceps',
+    name: 'Standing Biceps Cable Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_032',
+    muscle: 'Biceps',
+    name: 'Drag Curl'
+  }, {
+    equipment: 'Machine',
+    id: 'biceps_033',
+    muscle: 'Biceps',
+    name: 'Machine Preacher Curls'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_034',
+    muscle: 'Biceps',
+    name: 'Lying Supine Dumbbell Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_035',
+    muscle: 'Biceps',
+    name: 'Two-Arm Dumbbell Preacher Curl'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_036',
+    muscle: 'Biceps',
+    name: 'Lying Cable Curl'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_037',
+    muscle: 'Biceps',
+    name: 'Cable Hammer Curls - Rope Attachment'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_038',
+    muscle: 'Biceps',
+    name: 'Reverse Barbell Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_039',
+    muscle: 'Biceps',
+    name: 'Standing Dumbbell Reverse Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_040',
+    muscle: 'Biceps',
+    name: 'Incline Inner Biceps Curl'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_041',
+    muscle: 'Biceps',
+    name: 'High Cable Curls'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'biceps_042',
+    muscle: 'Biceps',
+    name: 'Close-Grip EZ-Bar Curl with Band'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_043',
+    muscle: 'Biceps',
+    name: 'One-Arm Plank Dumbbell Biceps Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_044',
+    muscle: 'Biceps',
+    name: 'Dumbbell Prone Incline Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_045',
+    muscle: 'Biceps',
+    name: 'Zottman Preacher Curl'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_046',
+    muscle: 'Biceps',
+    name: 'Cable Preacher Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_047',
+    muscle: 'Biceps',
+    name: 'Standing Inner-Biceps Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'biceps_048',
+    muscle: 'Biceps',
+    name: 'Seated Dumbbell Inner Biceps Curl'
+  }, {
+    equipment: 'Other',
+    id: 'biceps_049',
+    muscle: 'Biceps',
+    name: 'Reverse Plate Curls'
+  }, {
+    equipment: 'Cable',
+    id: 'biceps_050',
+    muscle: 'Biceps',
+    name: 'Lying Close-Grip Bar Curl On High Pulley'
+  }, {
+    equipment: 'Other',
+    id: 'biceps_051',
+    muscle: 'Biceps',
+    name: 'Standing Biceps Stretch'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_052',
+    muscle: 'Biceps',
+    name: 'Lying High Bench Barbell Curl'
+  }, {
+    equipment: 'Body Only',
+    id: 'biceps_053',
+    muscle: 'Biceps',
+    name: 'Seated Biceps'
+  }, {
+    equipment: 'Foam Roll',
+    id: 'biceps_054',
+    muscle: 'Biceps',
+    name: 'Brachialis-SMR'
+  }, {
+    equipment: 'Other',
+    id: 'biceps_055',
+    muscle: 'Biceps',
+    name: 'Suspended Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'biceps_056',
+    muscle: 'Biceps',
+    name: 'Seated Straight-Bar Curl Superset'
+  }, {
+    equipment: 'Other',
+    id: 'biceps_057',
+    muscle: 'Biceps',
+    name: 'Hammer Plate Curl'
+  }
+]
