9e5ba79880e731d44f074442c044807174921438
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:25:43 +0200

    Add library of calve exercises

---
+++ src/stores/store-exercises-calves.ts
@@ -0,0 +1,150 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesCalvesStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Machine',
+    id: 'calves_000',
+    muscle: 'Calves',
+    name: 'Smith Machine Calf Raise'
+  }, {
+    equipment: 'Machine',
+    id: 'calves_001',
+    muscle: 'Calves',
+    name: 'Standing Calf Raises'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'calves_002',
+    muscle: 'Calves',
+    name: 'Standing Dumbbell Calf Raise'
+  }, {
+    equipment: 'Machine',
+    id: 'calves_003',
+    muscle: 'Calves',
+    name: 'Seated Calf Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'calves_004',
+    muscle: 'Calves',
+    name: 'Rocking Standing Calf Raise'
+  }, {
+    equipment: 'Machine',
+    id: 'calves_005',
+    muscle: 'Calves',
+    name: 'Calf Press On The Leg Press Machine'
+  }, {
+    equipment: 'Machine',
+    id: 'calves_006',
+    muscle: 'Calves',
+    name: 'Calf Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'calves_007',
+    muscle: 'Calves',
+    name: 'Standing Barbell Calf Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'calves_008',
+    muscle: 'Calves',
+    name: 'Barbell Seated Calf Raise'
+  }, {
+    equipment: 'Other',
+    id: 'calves_009',
+    muscle: 'Calves',
+    name: 'Balance Board'
+  }, {
+    equipment: 'Other',
+    id: 'calves_010',
+    muscle: 'Calves',
+    name: 'Donkey Calf Raises'
+  }, {
+    equipment: 'Machine',
+    id: 'calves_011',
+    muscle: 'Calves',
+    name: 'Smith Machine Reverse Calf Raises'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'calves_012',
+    muscle: 'Calves',
+    name: 'Dumbbell Seated One-Leg Calf Raise'
+  }, {
+    equipment: 'None',
+    id: 'calves_013',
+    muscle: 'Calves',
+    name: 'Ankle Circles'
+  }, {
+    equipment: 'Foam Roll',
+    id: 'calves_014',
+    muscle: 'Calves',
+    name: 'Peroneals-SMR'
+  }, {
+    equipment: 'Body Only',
+    id: 'calves_015',
+    muscle: 'Calves',
+    name: 'Knee Circles'
+  }, {
+    equipment: 'Foam Roll',
+    id: 'calves_016',
+    muscle: 'Calves',
+    name: 'Calves-SMR'
+  }, {
+    equipment: 'None',
+    id: 'calves_017',
+    muscle: 'Calves',
+    name: 'Standing Gastrocnemius Calf Stretch'
+  }, {
+    equipment: 'None',
+    id: 'calves_018',
+    muscle: 'Calves',
+    name: 'Calf Stretch Hands Against Wall'
+  }, {
+    equipment: 'Other',
+    id: 'calves_019',
+    muscle: 'Calves',
+    name: 'Anterior Tibialis-SMR'
+  }, {
+    equipment: 'None',
+    id: 'calves_020',
+    muscle: 'Calves',
+    name: 'Calf Stretch Elbows Against Wall'
+  }, {
+    equipment: 'Bands',
+    id: 'calves_021',
+    muscle: 'Calves',
+    name: 'Calf Raises - With Bands'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'calves_022',
+    muscle: 'Calves',
+    name: 'Calf Raise On A Dumbbell'
+  }, {
+    equipment: 'None',
+    id: 'calves_023',
+    muscle: 'Calves',
+    name: 'Standing Soleus And Achilles Stretch'
+  }, {
+    equipment: 'None',
+    id: 'calves_024',
+    muscle: 'Calves',
+    name: 'Seated Calf Stretch'
+  }, {
+    equipment: 'Other',
+    id: 'calves_025',
+    muscle: 'Calves',
+    name: 'Peroneals Stretch'
+  }, {
+    equipment: 'Other',
+    id: 'calves_026',
+    muscle: 'Calves',
+    name: 'Foot-SMR'
+  }, {
+    equipment: 'Other',
+    id: 'calves_027',
+    muscle: 'Calves',
+    name: 'Posterior Tibialis Stretch'
+  }
+]
