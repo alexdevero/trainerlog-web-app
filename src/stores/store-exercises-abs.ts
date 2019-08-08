d598ca714d016e6c2121e4b172c2de1f900045cd
Alex Devero <deveroalex@gmail.com>
2019-08-08 15:17:20 +0200

    Add library of abs exercises

---
+++ src/stores/store-exercises-abs.ts
@@ -0,0 +1,635 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesAbsStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Medicine Ball',
+    id: 'abs_000',
+    muscle: 'Abdominals',
+    name: 'One-Arm Medicine Ball Slam'
+  }, {
+    equipment: 'Barbell',
+    id: 'abs_001',
+    muscle: 'Abdominals',
+    name: 'Landmine 180\'s'
+  }, {
+    equipment: 'Other',
+    id: 'abs_002',
+    muscle: 'Abdominals',
+    name: 'Suspended Fallout'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_003',
+    muscle: 'Abdominals',
+    name: 'Plank'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_004',
+    muscle: 'Abdominals',
+    name: 'Standing Cable Lift'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_005',
+    muscle: 'Abdominals',
+    name: 'Bottoms Up'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'abs_006',
+    muscle: 'Abdominals',
+    name: 'Spell Caster'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'abs_007',
+    muscle: 'Abdominals',
+    name: 'Dumbbell V-Sit Cross Jab'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_008',
+    muscle: 'Abdominals',
+    name: 'Decline Reverse Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_009',
+    muscle: 'Abdominals',
+    name: 'Spider Crawl'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_010',
+    muscle: 'Abdominals',
+    name: 'Cross-Body Crunch'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_011',
+    muscle: 'Abdominals',
+    name: 'One-Arm High-Pulley Cable Side Bends'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_012',
+    muscle: 'Abdominals',
+    name: 'Elbow to Knee'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_013',
+    muscle: 'Abdominals',
+    name: 'Cocoons'
+  }, {
+    equipment: 'Other',
+    id: 'abs_014',
+    muscle: 'Abdominals',
+    name: 'Plate Twist'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_015',
+    muscle: 'Abdominals',
+    name: 'Gorilla Chin/Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_016',
+    muscle: 'Abdominals',
+    name: 'Decline Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_017',
+    muscle: 'Abdominals',
+    name: 'Hanging Pike'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_018',
+    muscle: 'Abdominals',
+    name: 'Hanging Oblique Knee Raise'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_019',
+    muscle: 'Abdominals',
+    name: 'Weighted Suitcase Crunch'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_020',
+    muscle: 'Abdominals',
+    name: 'Oblique Cable Crunch'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'abs_021',
+    muscle: 'Abdominals',
+    name: 'Exercise Ball Pull-In'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_022',
+    muscle: 'Abdominals',
+    name: 'Cable Crunch'
+  }, {
+    equipment: 'Barbell',
+    id: 'abs_023',
+    muscle: 'Abdominals',
+    name: 'Barbell Ab Rollout'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_024',
+    muscle: 'Abdominals',
+    name: 'Hanging Leg Raise'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_025',
+    muscle: 'Abdominals',
+    name: 'Kneeling Cable Crunch With Alternating Oblique Twists'
+  }, {
+    equipment: 'Other',
+    id: 'abs_026',
+    muscle: 'Abdominals',
+    name: 'Ab Roller'
+  }, {
+    equipment: 'Barbell',
+    id: 'abs_027',
+    muscle: 'Abdominals',
+    name: 'Barbell Ab Rollout - On Knees'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_028',
+    muscle: 'Abdominals',
+    name: 'Weighted Crunches'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_029',
+    muscle: 'Abdominals',
+    name: 'Reverse Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_030',
+    muscle: 'Abdominals',
+    name: 'Air Bike'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_031',
+    muscle: 'Abdominals',
+    name: '3/4 Sit-Up'
+  }, {
+    equipment: 'Other',
+    id: 'abs_032',
+    muscle: 'Abdominals',
+    name: 'Otis-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_033',
+    muscle: 'Abdominals',
+    name: 'Crunches'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'abs_034',
+    muscle: 'Abdominals',
+    name: 'Dumbbell Side Bend'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_035',
+    muscle: 'Abdominals',
+    name: 'Rope Crunch'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_036',
+    muscle: 'Abdominals',
+    name: 'Standing Rope Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_037',
+    muscle: 'Abdominals',
+    name: 'Dead Bug'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_038',
+    muscle: 'Abdominals',
+    name: 'Crunch - Hands Overhead'
+  }, {
+    equipment: 'Other',
+    id: 'abs_039',
+    muscle: 'Abdominals',
+    name: 'Knee/Hip Raise On Parallel Bars'
+  }, {
+    equipment: 'Other',
+    id: 'abs_040',
+    muscle: 'Abdominals',
+    name: 'Sledgehammer Swings'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_041',
+    muscle: 'Abdominals',
+    name: 'Jackknife Sit-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_042',
+    muscle: 'Abdominals',
+    name: 'Decline Oblique Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_043',
+    muscle: 'Abdominals',
+    name: 'Stomach Vacuum'
+  }, {
+    equipment: 'Barbell',
+    id: 'abs_044',
+    muscle: 'Abdominals',
+    name: 'Press Sit-Up'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_045',
+    muscle: 'Abdominals',
+    name: 'Bosu Ball Cable Crunch With Side Bends'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_046',
+    muscle: 'Abdominals',
+    name: 'Tuck Crunch'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_047',
+    muscle: 'Abdominals',
+    name: 'Pallof Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_048',
+    muscle: 'Abdominals',
+    name: 'Flat Bench Lying Leg Raise'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_049',
+    muscle: 'Abdominals',
+    name: 'Seated Flat Bench Leg Pull-In'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'abs_050',
+    muscle: 'Abdominals',
+    name: 'Exercise Ball Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_051',
+    muscle: 'Abdominals',
+    name: 'Russian Twist'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_052',
+    muscle: 'Abdominals',
+    name: 'Oblique Crunches'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'abs_053',
+    muscle: 'Abdominals',
+    name: 'Advanced Kettlebell Windmill'
+  }, {
+    equipment: 'Barbell',
+    id: 'abs_054',
+    muscle: 'Abdominals',
+    name: 'Barbell Rollout from Bench'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_055',
+    muscle: 'Abdominals',
+    name: 'Butt-Ups'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_056',
+    muscle: 'Abdominals',
+    name: 'Cable Reverse Crunch'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_057',
+    muscle: 'Abdominals',
+    name: 'Standing Cable Wood Chop'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_058',
+    muscle: 'Abdominals',
+    name: 'Bent-Knee Hip Raise'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_059',
+    muscle: 'Abdominals',
+    name: 'Cable Russian Twists'
+  }, {
+    equipment: 'Machine',
+    id: 'abs_060',
+    muscle: 'Abdominals',
+    name: 'Ab Crunch Machine'
+  }, {
+    equipment: 'Other',
+    id: 'abs_061',
+    muscle: 'Abdominals',
+    name: 'Weighted Sit-Ups - With Bands'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_062',
+    muscle: 'Abdominals',
+    name: 'Wind Sprints'
+  }, {
+    equipment: 'Machine',
+    id: 'abs_063',
+    muscle: 'Abdominals',
+    name: 'Smith Machine Hip Raise'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_064',
+    muscle: 'Abdominals',
+    name: 'Alternate Heel Touchers'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_065',
+    muscle: 'Abdominals',
+    name: 'Pallof Press With Rotation'
+  }, {
+    equipment: 'Other',
+    id: 'abs_066',
+    muscle: 'Abdominals',
+    name: 'Suspended Reverse Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_067',
+    muscle: 'Abdominals',
+    name: 'Flat Bench Leg Pull-In'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_068',
+    muscle: 'Abdominals',
+    name: 'Cable Seated Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_069',
+    muscle: 'Abdominals',
+    name: 'Frog Sit-Ups'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'abs_070',
+    muscle: 'Abdominals',
+    name: 'Kettlebell Windmill'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'abs_071',
+    muscle: 'Abdominals',
+    name: 'Kettlebell Figure 8'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_072',
+    muscle: 'Abdominals',
+    name: 'Sit-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_073',
+    muscle: 'Abdominals',
+    name: 'Side Bridge'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_074',
+    muscle: 'Abdominals',
+    name: 'Leg Pull-In'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_075',
+    muscle: 'Abdominals',
+    name: 'Oblique Crunches - On The Floor'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_076',
+    muscle: 'Abdominals',
+    name: 'Janda Sit-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_077',
+    muscle: 'Abdominals',
+    name: 'Crunch - Legs On Exercise Ball'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_078',
+    muscle: 'Abdominals',
+    name: 'Side Jackknife'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'abs_079',
+    muscle: 'Abdominals',
+    name: 'Kettlebell Pass Between The Legs'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_080',
+    muscle: 'Abdominals',
+    name: 'Seated Leg Tucks'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_081',
+    muscle: 'Abdominals',
+    name: 'Scissor Kick'
+  }, {
+    equipment: 'Barbell',
+    id: 'abs_082',
+    muscle: 'Abdominals',
+    name: 'Barbell Side Bend'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_083',
+    muscle: 'Abdominals',
+    name: 'Toe Touchers'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_084',
+    muscle: 'Abdominals',
+    name: 'Lower Back Curl'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'abs_085',
+    muscle: 'Abdominals',
+    name: 'Weighted Ball Side Bend'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'abs_086',
+    muscle: 'Abdominals',
+    name: 'Bent Press'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_087',
+    muscle: 'Abdominals',
+    name: 'Supine Two-Arm Overhead Throw'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'abs_088',
+    muscle: 'Abdominals',
+    name: 'Torso Rotation'
+  }, {
+    equipment: 'Cable',
+    id: 'abs_089',
+    muscle: 'Abdominals',
+    name: 'Cable Judo Flip'
+  }, {
+    equipment: 'Barbell',
+    id: 'abs_090',
+    muscle: 'Abdominals',
+    name: 'Seated Barbell Twist'
+  }, {
+    equipment: 'None',
+    id: 'abs_091',
+    muscle: 'Abdominals',
+    name: 'Seated Overhead Stretch'
+  }, {
+    equipment: 'None',
+    id: 'abs_092',
+    muscle: 'Abdominals',
+    name: 'Overhead Stretch'
+  }, {
+    equipment: 'None',
+    id: 'abs_093',
+    muscle: 'Abdominals',
+    name: 'Standing Lateral Stretch'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'abs_094',
+    muscle: 'Abdominals',
+    name: 'Double Kettlebell Windmill'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_095',
+    muscle: 'Abdominals',
+    name: 'Hip Crossover'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_096',
+    muscle: 'Abdominals',
+    name: 'Plank with Twist'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_097',
+    muscle: 'Abdominals',
+    name: 'Suitcase Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_098',
+    muscle: 'Abdominals',
+    name: 'Seated Scissor Kick'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_099',
+    muscle: 'Abdominals',
+    name: 'Cross Crunch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_100',
+    muscle: 'Abdominals',
+    name: 'Straight-Legged Hip Raise'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_101',
+    muscle: 'Abdominals',
+    name: 'Partner Facing Planks With Alternating High-Five'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_102',
+    muscle: 'Abdominals',
+    name: 'Partner 3-Touch Motion Russian Twist'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_103',
+    muscle: 'Abdominals',
+    name: 'Partner Side-To-Side Russian Twist & Pass'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_104',
+    muscle: 'Abdominals',
+    name: 'Partner Hanging Knee Raise With Throw Down'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_105',
+    muscle: 'Abdominals',
+    name: 'Partner Hanging Knee Raise With Manual Resistance'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_106',
+    muscle: 'Abdominals',
+    name: 'Medicine Ball Rotational Throw'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_107',
+    muscle: 'Abdominals',
+    name: 'Suspended Crunch'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'abs_108',
+    muscle: 'Abdominals',
+    name: 'Bosu Ball Crunch'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'abs_109',
+    muscle: 'Abdominals',
+    name: 'Stability Ball Pike With Knee Tuck'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_110',
+    muscle: 'Abdominals',
+    name: 'Shoulder Tap'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_111',
+    muscle: 'Abdominals',
+    name: 'Kettlebell 3-Point Extension'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_112',
+    muscle: 'Abdominals',
+    name: 'Breakdancer'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_113',
+    muscle: 'Abdominals',
+    name: 'Medicine Ball Full Twist'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_114',
+    muscle: 'Abdominals',
+    name: 'Supine One-Arm Overhead Throw'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_115',
+    muscle: 'Abdominals',
+    name: 'Partner Target Sit-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_116',
+    muscle: 'Abdominals',
+    name: 'Partner Lying Leg Raise With Throw Down'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_117',
+    muscle: 'Abdominals',
+    name: 'Partner Sit-Up With High-Five'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_118',
+    muscle: 'Abdominals',
+    name: 'Partner Lying Leg Raise With Lateral Throw Down'
+  }, {
+    equipment: 'Other',
+    id: 'abs_119',
+    muscle: 'Abdominals',
+    name: 'Waiter\'s Carry'
+  }, {
+    equipment: 'Other',
+    id: 'abs_120',
+    muscle: 'Abdominals',
+    name: 'Skin The Cat To Push-Up'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'abs_121',
+    muscle: 'Abdominals',
+    name: 'V-Sit Lying Down Ball Throw And Catch'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_122',
+    muscle: 'Abdominals',
+    name: 'Wall Mountain Climber'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_123',
+    muscle: 'Abdominals',
+    name: 'Full Moon'
+  }, {
+    equipment: 'Body Only',
+    id: 'abs_124',
+    muscle: 'Abdominals',
+    name: 'Crab Toe Touch'
+  }
+]
