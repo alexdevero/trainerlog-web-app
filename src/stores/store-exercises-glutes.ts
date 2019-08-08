c8b0af0318e54f5648dfe1ee9774e97f27427c4e
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:53:53 +0200

    Add library of glutes exercises

---
+++ src/stores/store-exercises-glutes.ts
@@ -0,0 +1,140 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesGlutesStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Barbell',
+    id: 'glutes_000',
+    muscle: 'Glutes',
+    name: 'Barbell Glute Bridge'
+  }, {
+    equipment: 'Barbell',
+    id: 'glutes_001',
+    muscle: 'Glutes',
+    name: 'Barbell Hip Thrust'
+  }, {
+    equipment: 'Cable',
+    id: 'glutes_002',
+    muscle: 'Glutes',
+    name: 'One-Legged Cable Kickback'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_003',
+    muscle: 'Glutes',
+    name: 'Butt Lift (Bridge)'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_004',
+    muscle: 'Glutes',
+    name: 'Single Leg Glute Bridge'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_005',
+    muscle: 'Glutes',
+    name: 'Step-up with Knee Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'glutes_006',
+    muscle: 'Glutes',
+    name: 'Kneeling Squat'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_007',
+    muscle: 'Glutes',
+    name: 'Glute Kickback'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_008',
+    muscle: 'Glutes',
+    name: 'Flutter Kicks'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'glutes_009',
+    muscle: 'Glutes',
+    name: 'Physioball Hip Bridge'
+  }, {
+    equipment: 'Barbell',
+    id: 'glutes_010',
+    muscle: 'Glutes',
+    name: 'Kneeling Jump Squat'
+  }, {
+    equipment: 'Cable',
+    id: 'glutes_011',
+    muscle: 'Glutes',
+    name: 'Pull Through'
+  }, {
+    equipment: 'Bands',
+    id: 'glutes_012',
+    muscle: 'Glutes',
+    name: 'Hip Extension with Bands'
+  }, {
+    equipment: 'Foam Roll',
+    id: 'glutes_013',
+    muscle: 'Glutes',
+    name: 'Piriformis-SMR'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_014',
+    muscle: 'Glutes',
+    name: 'Leg Lift'
+  }, {
+    equipment: 'Bands',
+    id: 'glutes_015',
+    muscle: 'Glutes',
+    name: 'Hip Lift with Band'
+  }, {
+    equipment: 'None',
+    id: 'glutes_016',
+    muscle: 'Glutes',
+    name: 'Ankle On The Knee'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'glutes_017',
+    muscle: 'Glutes',
+    name: 'Downward Facing Balance'
+  }, {
+    equipment: 'None',
+    id: 'glutes_018',
+    muscle: 'Glutes',
+    name: 'Knee Across The Body'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_019',
+    muscle: 'Glutes',
+    name: 'Seated Glute'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_020',
+    muscle: 'Glutes',
+    name: 'Lying Glute'
+  }, {
+    equipment: 'None',
+    id: 'glutes_021',
+    muscle: 'Glutes',
+    name: 'One Knee To Chest'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_022',
+    muscle: 'Glutes',
+    name: 'Glute Bridge Hamstring Walkout'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_023',
+    muscle: 'Glutes',
+    name: 'Neck Bridge Supine'
+  }, {
+    equipment: 'Body Only',
+    id: 'glutes_024',
+    muscle: 'Glutes',
+    name: 'Seated Glute Stretch'
+  }, {
+    equipment: 'Other',
+    id: 'glutes_025',
+    muscle: 'Glutes',
+    name: 'Suspended Hip Thrust'
+  }
+]
