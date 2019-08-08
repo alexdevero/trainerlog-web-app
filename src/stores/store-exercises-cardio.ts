e836215156d4dbb473a57eb36028270db36e6319
Alex Devero <deveroalex@gmail.com>
2019-08-08 17:15:02 +0200

    Add library of cardio exercises

---
+++ src/stores/store-exercises-cardio.ts
@@ -0,0 +1,155 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesCardioStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Other',
+    id: 'cardio_000',
+    muscle: 'Quadriceps',
+    name: 'Rope Jumping'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_001',
+    muscle: 'Quadriceps',
+    name: 'Step Mill'
+  }, {
+    equipment: 'Other',
+    id: 'cardio_002',
+    muscle: 'Quadriceps',
+    name: 'Bicycling'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_003',
+    muscle: 'Quadriceps',
+    name: 'Rowing, Stationary'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_004',
+    muscle: 'Quadriceps',
+    name: 'Elliptical Trainer'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_005',
+    muscle: 'Quadriceps',
+    name: 'Stairmaster'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_006',
+    muscle: 'Quadriceps',
+    name: 'Burpee'
+  }, {
+    equipment: 'None',
+    id: 'cardio_007',
+    muscle: 'Quadriceps',
+    name: 'Trail Running/Walking'
+  }, {
+    equipment: 'Other',
+    id: 'cardio_008',
+    muscle: 'Quadriceps',
+    name: 'Skating'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_009',
+    muscle: 'Quadriceps',
+    name: 'Jog In Place'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_010',
+    muscle: 'Quadriceps',
+    name: 'Running, Treadmill'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_011',
+    muscle: 'Quadriceps',
+    name: 'Bicycling, Stationary'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_012',
+    muscle: 'Quadriceps',
+    name: 'Jogging-Treadmill'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_013',
+    muscle: 'Quadriceps',
+    name: 'Recumbent Bike'
+  }, {
+    equipment: 'Machine',
+    id: 'cardio_014',
+    muscle: 'Quadriceps',
+    name: 'Walking, Treadmill'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_015',
+    muscle: 'Quadriceps',
+    name: 'Butt Kicks'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_016',
+    muscle: 'Quadriceps',
+    name: 'Walking High Knees'
+  }, {
+    equipment: 'Other',
+    id: 'cardio_017',
+    muscle: 'Hamstrings',
+    name: 'Prowler Sprint'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_018',
+    muscle: 'Shoulders',
+    name: 'Punches'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_019',
+    muscle: 'Quadriceps',
+    name: 'Slow Jog'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_020',
+    muscle: 'Quadriceps',
+    name: 'Defensive Slide'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_021',
+    muscle: 'Quadriceps',
+    name: 'Lateral Speed Step'
+  }, {
+    equipment: 'Other',
+    id: 'cardio_022',
+    muscle: 'Quadriceps',
+    name: 'Double Under'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_023',
+    muscle: 'Quadriceps',
+    name: 'Jumping Jack'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_024',
+    muscle: 'Shoulders',
+    name: 'Bear Crawl Fire Feet'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_025',
+    muscle: 'Quadriceps',
+    name: 'Fast Kick With Arm Circles'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_026',
+    muscle: 'Quadriceps',
+    name: 'Football Up-Down'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_027',
+    muscle: 'Quadriceps',
+    name: 'Vertical Mountain Climber'
+  }, {
+    equipment: 'Body Only',
+    id: 'cardio_028',
+    muscle: 'Quadriceps',
+    name: 'High Knee Jog'
+  }
+]
