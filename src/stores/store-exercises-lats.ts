315c679287074d733dbd3cadc531c0cab9ddb255
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:54:01 +0200

    Add library of lats exercises

---
+++ src/stores/store-exercises-lats.ts
@@ -0,0 +1,245 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesLatsStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Other',
+    id: 'lats_000',
+    muscle: 'Lats',
+    name: 'Weighted Pull Ups'
+  }, {
+    equipment: 'Body Only',
+    id: 'lats_001',
+    muscle: 'Lats',
+    name: 'Pullups'
+  }, {
+    equipment: 'Body Only',
+    id: 'lats_002',
+    muscle: 'Lats',
+    name: 'Chin-Up'
+  }, {
+    equipment: 'Other',
+    id: 'lats_003',
+    muscle: 'Lats',
+    name: 'Rocky Pull-Ups/Pulldowns'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_004',
+    muscle: 'Lats',
+    name: 'V-Bar Pulldown'
+  }, {
+    equipment: 'Other',
+    id: 'lats_005',
+    muscle: 'Lats',
+    name: 'Muscle Up'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_006',
+    muscle: 'Lats',
+    name: 'Shotgun Row'
+  }, {
+    equipment: 'Body Only',
+    id: 'lats_007',
+    muscle: 'Lats',
+    name: 'Wide-Grip Pull-Up'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_008',
+    muscle: 'Lats',
+    name: 'Close-Grip Front Lat Pulldown'
+  }, {
+    equipment: 'Body Only',
+    id: 'lats_009',
+    muscle: 'Lats',
+    name: 'V-Bar Pullup'
+  }, {
+    equipment: 'Body Only',
+    id: 'lats_010',
+    muscle: 'Lats',
+    name: 'Wide-Grip Rear Pull-Up'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_011',
+    muscle: 'Lats',
+    name: 'Rope Straight-Arm Pulldown'
+  }, {
+    equipment: 'Other',
+    id: 'lats_012',
+    muscle: 'Lats',
+    name: 'Rope Climb'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_013',
+    muscle: 'Lats',
+    name: 'Wide-Grip Lat Pulldown'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_014',
+    muscle: 'Lats',
+    name: 'Underhand Cable Pulldowns'
+  }, {
+    equipment: 'Other',
+    id: 'lats_015',
+    muscle: 'Lats',
+    name: 'Gironda Sternum Chins'
+  }, {
+    equipment: 'Other',
+    id: 'lats_016',
+    muscle: 'Lats',
+    name: 'Side To Side Chins'
+  }, {
+    equipment: 'Machine',
+    id: 'lats_017',
+    muscle: 'Lats',
+    name: 'Leverage Iso Row'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_018',
+    muscle: 'Lats',
+    name: 'Straight-Arm Pulldown'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_019',
+    muscle: 'Lats',
+    name: 'Elevated Cable Rows'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_020',
+    muscle: 'Lats',
+    name: 'Kneeling Single-Arm High Pulley Row'
+  }, {
+    equipment: 'Body Only',
+    id: 'lats_021',
+    muscle: 'Lats',
+    name: 'Neutral-Grip Pull Ups'
+  }, {
+    equipment: 'Machine',
+    id: 'lats_022',
+    muscle: 'Lats',
+    name: 'Machine-Assisted Pull-Up'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_023',
+    muscle: 'Lats',
+    name: 'Full Range-Of-Motion Lat Pulldown'
+  }, {
+    equipment: 'Barbell',
+    id: 'lats_024',
+    muscle: 'Lats',
+    name: 'Bent-Arm Barbell Pullover'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'lats_025',
+    muscle: 'Lats',
+    name: 'Overhead Slam'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_026',
+    muscle: 'Lats',
+    name: 'Kneeling High Pulley Row'
+  }, {
+    equipment: 'Other',
+    id: 'lats_027',
+    muscle: 'Lats',
+    name: 'Band Assisted Pull-Up'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_028',
+    muscle: 'Lats',
+    name: 'One Arm Lat Pulldown'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_029',
+    muscle: 'Lats',
+    name: 'Cable Incline Pushdown'
+  }, {
+    equipment: 'Other',
+    id: 'lats_030',
+    muscle: 'Lats',
+    name: 'Kipping Muscle Up'
+  }, {
+    equipment: 'Cable',
+    id: 'lats_031',
+    muscle: 'Lats',
+    name: 'Wide-Grip Pulldown Behind The Neck'
+  }, {
+    equipment: 'Other',
+    id: 'lats_032',
+    muscle: 'Lats',
+    name: 'London Bridges'
+  }, {
+    equipment: 'None',
+    id: 'lats_033',
+    muscle: 'Lats',
+    name: 'One Arm Against Wall'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'lats_034',
+    muscle: 'Lats',
+    name: 'Catch and Overhead Throw'
+  }, {
+    equipment: 'None',
+    id: 'lats_035',
+    muscle: 'Lats',
+    name: 'Dynamic Back Stretch'
+  }, {
+    equipment: 'Foam Roll',
+    id: 'lats_036',
+    muscle: 'Lats',
+    name: 'Latissimus Dorsi-SMR'
+  }, {
+    equipment: 'Other',
+    id: 'lats_037',
+    muscle: 'Lats',
+    name: 'One Handed Hang'
+  }, {
+    equipment: 'None',
+    id: 'lats_038',
+    muscle: 'Lats',
+    name: 'Chair Lower Back Stretch'
+  }, {
+    equipment: 'Other',
+    id: 'lats_039',
+    muscle: 'Lats',
+    name: 'Overhead Lat'
+  }, {
+    equipment: 'None',
+    id: 'lats_040',
+    muscle: 'Lats',
+    name: 'Side-Lying Floor Stretch'
+  }, {
+    equipment: 'Barbell',
+    id: 'lats_041',
+    muscle: 'Lats',
+    name: 'Rockers (Pullover To Press) Straight Bar'
+  }, {
+    equipment: 'Bands',
+    id: 'lats_042',
+    muscle: 'Lats',
+    name: 'Assisted Chin-Up'
+  }, {
+    equipment: 'Other',
+    id: 'lats_043',
+    muscle: 'Lats',
+    name: 'Around The World Pull-Up'
+  }, {
+    equipment: 'Other',
+    id: 'lats_044',
+    muscle: 'Lats',
+    name: 'Burpee Pull-Up'
+  }, {
+    equipment: 'Other',
+    id: 'lats_045',
+    muscle: 'Lats',
+    name: 'Negative Pull-Up'
+  }, {
+    equipment: 'Other',
+    id: 'lats_046',
+    muscle: 'Lats',
+    name: 'L-Sit Chin-Up'
+  }
+]
