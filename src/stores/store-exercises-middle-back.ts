a1d21d8ce33a95f4c8d0a4eb58e2b617b78e64e6
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:54:17 +0200

    Add library of middle back exercises

---
+++ src/stores/store-exercises-middle-back.ts
@@ -0,0 +1,235 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesMiddleBackStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Barbell',
+    id: 'middle_back_000',
+    muscle: 'Middle Back',
+    name: 'T-Bar Row with Handle'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_001',
+    muscle: 'Middle Back',
+    name: 'Reverse Grip Bent-Over Rows'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_002',
+    muscle: 'Middle Back',
+    name: 'One-Arm Long Bar Row'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'middle_back_003',
+    muscle: 'Middle Back',
+    name: 'One-Arm Dumbbell Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_004',
+    muscle: 'Middle Back',
+    name: 'Bent Over One-Arm Long Bar Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_005',
+    muscle: 'Middle Back',
+    name: 'T-Bar Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_006',
+    muscle: 'Middle Back',
+    name: 'Bent Over Two-Arm Long Bar Row'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'middle_back_007',
+    muscle: 'Middle Back',
+    name: 'Dumbbell Incline Row'
+  }, {
+    equipment: 'Other',
+    id: 'middle_back_008',
+    muscle: 'Middle Back',
+    name: 'Bodyweight Mid Row'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'middle_back_009',
+    muscle: 'Middle Back',
+    name: 'Bent Over Two-Dumbbell Row With Palms In'
+  }, {
+    equipment: 'Cable',
+    id: 'middle_back_010',
+    muscle: 'Middle Back',
+    name: 'Seated Cable Rows'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_011',
+    muscle: 'Middle Back',
+    name: 'Bent Over Barbell Row'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'middle_back_012',
+    muscle: 'Middle Back',
+    name: 'Alternating Renegade Row'
+  }, {
+    equipment: 'Cable',
+    id: 'middle_back_013',
+    muscle: 'Middle Back',
+    name: 'Seated One-arm Cable Pulley Rows'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'middle_back_014',
+    muscle: 'Middle Back',
+    name: 'Man Maker'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'middle_back_015',
+    muscle: 'Middle Back',
+    name: 'Bent Over Two-Dumbbell Row'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'middle_back_016',
+    muscle: 'Middle Back',
+    name: 'Middle Back Shrug'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_017',
+    muscle: 'Middle Back',
+    name: 'Yates Row Reverse Grip'
+  }, {
+    equipment: 'Other',
+    id: 'middle_back_018',
+    muscle: 'Middle Back',
+    name: 'Mixed Grip Chin'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'middle_back_019',
+    muscle: 'Middle Back',
+    name: 'One-Arm Kettlebell Row'
+  }, {
+    equipment: 'None',
+    id: 'middle_back_020',
+    muscle: 'Middle Back',
+    name: 'Inverted Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_021',
+    muscle: 'Middle Back',
+    name: 'Pendlay Rown'
+  }, {
+    equipment: 'Machine',
+    id: 'middle_back_022',
+    muscle: 'Middle Back',
+    name: 'Smith Machine Bent Over Row'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'middle_back_023',
+    muscle: 'Middle Back',
+    name: 'Alternating Kettlebell Row'
+  }, {
+    equipment: 'Machine',
+    id: 'middle_back_024',
+    muscle: 'Middle Back',
+    name: 'Lying T-Bar Row'
+  }, {
+    equipment: 'Machine',
+    id: 'middle_back_025',
+    muscle: 'Middle Back',
+    name: 'Leverage High Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_026',
+    muscle: 'Middle Back',
+    name: 'Incline Bench Pull'
+  }, {
+    equipment: 'Other',
+    id: 'middle_back_027',
+    muscle: 'Middle Back',
+    name: 'Suspended Row'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'middle_back_028',
+    muscle: 'Middle Back',
+    name: 'Two-Arm Kettlebell Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_029',
+    muscle: 'Middle Back',
+    name: 'Straight Bar Bench Mid Rows'
+  }, {
+    equipment: 'Other',
+    id: 'middle_back_030',
+    muscle: 'Middle Back',
+    name: 'Inverted Row with Straps'
+  }, {
+    equipment: 'Other',
+    id: 'middle_back_031',
+    muscle: 'Middle Back',
+    name: 'One Arm Chin-Up'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_032',
+    muscle: 'Middle Back',
+    name: 'Lying Cambered Barbell Row'
+  }, {
+    equipment: 'Foam Roll',
+    id: 'middle_back_033',
+    muscle: 'Middle Back',
+    name: 'Rhomboids-SMR'
+  }, {
+    equipment: 'None',
+    id: 'middle_back_034',
+    muscle: 'Middle Back',
+    name: 'Spinal Stretch'
+  }, {
+    equipment: 'None',
+    id: 'middle_back_035',
+    muscle: 'Middle Back',
+    name: 'Upper Back Stretch'
+  }, {
+    equipment: 'None',
+    id: 'middle_back_036',
+    muscle: 'Middle Back',
+    name: 'Middle Back Stretch'
+  }, {
+    equipment: 'Bands',
+    id: 'middle_back_037',
+    muscle: 'Middle Back',
+    name: 'Partner Facing Side Plank With Band Row'
+  }, {
+    equipment: 'Bands',
+    id: 'middle_back_038',
+    muscle: 'Middle Back',
+    name: 'Partner Facing Feet-Elevated Side Plank With Band Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_039',
+    muscle: 'Middle Back',
+    name: 'Single-Arm Landmine Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_040',
+    muscle: 'Middle Back',
+    name: 'Barbell Deadlift Bent Row Complex'
+  }, {
+    equipment: 'Other',
+    id: 'middle_back_041',
+    muscle: 'Middle Back',
+    name: 'Sled Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_042',
+    muscle: 'Middle Back',
+    name: 'Plate Row'
+  }, {
+    equipment: 'Bands',
+    id: 'middle_back_043',
+    muscle: 'Middle Back',
+    name: 'Partner Facing Plank With Band Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'middle_back_044',
+    muscle: 'Middle Back',
+    name: 'Yates Row'
+  }
+]
