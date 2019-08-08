347b5efa467dbfe4853ae7fe62d1cba7c1c7b2b1
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:53:46 +0200

    Add library of forearms exercises

---
+++ src/stores/store-exercises-forearms.ts
@@ -0,0 +1,150 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesForearmsStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Other',
+    id: 'forearms_000',
+    muscle: 'Forearms',
+    name: 'Rickshaw Carry'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_001',
+    muscle: 'Forearms',
+    name: 'Wrist Rotations with Straight Bar'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_002',
+    muscle: 'Forearms',
+    name: 'Palms-Down Wrist Curl Over A Bench'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_003',
+    muscle: 'Forearms',
+    name: 'Palms-Up Barbell Wrist Curl Over A Bench'
+  }, {
+    equipment: 'Other',
+    id: 'forearms_004',
+    muscle: 'Forearms',
+    name: 'Farmer\'s Walk'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_005',
+    muscle: 'Forearms',
+    name: 'Standing Palms-Up Barbell Behind The Back Wrist Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_006',
+    muscle: 'Forearms',
+    name: 'Finger Curls'
+  }, {
+    equipment: 'Cable',
+    id: 'forearms_007',
+    muscle: 'Forearms',
+    name: 'Seated Two-Arm Palms-Up Low-Pulley Wrist Curl'
+  }, {
+    equipment: 'Other',
+    id: 'forearms_008',
+    muscle: 'Forearms',
+    name: 'Wrist Roller'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_009',
+    muscle: 'Forearms',
+    name: 'Seated One-Arm Dumbbell Palms-Up Wrist Curl'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_010',
+    muscle: 'Forearms',
+    name: 'Seated Palms-Down Barbell Wrist Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_011',
+    muscle: 'Forearms',
+    name: 'Dumbbell Lying Supination'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_012',
+    muscle: 'Forearms',
+    name: 'Dumbbell Lying Pronation'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_013',
+    muscle: 'Forearms',
+    name: 'Seated Palm-Up Barbell Wrist Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_014',
+    muscle: 'Forearms',
+    name: 'Palms-Up Dumbbell Wrist Curl Over A Bench'
+  }, {
+    equipment: 'Other',
+    id: 'forearms_015',
+    muscle: 'Forearms',
+    name: 'Standing Olympic Plate Hand Squeeze'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_016',
+    muscle: 'Forearms',
+    name: 'Seated Dumbbell Palms-Down Wrist Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_017',
+    muscle: 'Forearms',
+    name: 'Palms-Down Dumbbell Wrist Curl Over A Bench'
+  }, {
+    equipment: 'Other',
+    id: 'forearms_018',
+    muscle: 'Forearms',
+    name: 'Plate Pinch'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_019',
+    muscle: 'Forearms',
+    name: 'Seated One-Arm Dumbbell Palms-Down Wrist Curl'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'forearms_020',
+    muscle: 'Forearms',
+    name: 'Bottoms-Up Clean From The Hang Position'
+  }, {
+    equipment: 'Cable',
+    id: 'forearms_021',
+    muscle: 'Forearms',
+    name: 'Cable Wrist Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_022',
+    muscle: 'Forearms',
+    name: 'Seated Dumbbell Palms-Up Wrist Curl'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_023',
+    muscle: 'Forearms',
+    name: 'Partner Farmer\'s Walk Competition'
+  }, {
+    equipment: 'None',
+    id: 'forearms_024',
+    muscle: 'Forearms',
+    name: 'Kneeling Forearm Stretch'
+  }, {
+    equipment: 'Body Only',
+    id: 'forearms_025',
+    muscle: 'Forearms',
+    name: 'Wrist Circles'
+  }, {
+    equipment: 'Barbell',
+    id: 'forearms_026',
+    muscle: 'Forearms',
+    name: 'Partner Suitcase Carry Competition'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'forearms_027',
+    muscle: 'Forearms',
+    name: 'Suitcase Dumbbell Carry'
+  }
+]
