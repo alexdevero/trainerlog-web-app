2dcffb590b9874b8fac47f0668bdcd7192d4b9c7
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:54:45 +0200

    Add library of traps exercises

---
+++ src/stores/store-exercises-traps.ts
@@ -0,0 +1,90 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesTrapsStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Machine',
+    id: 'traps_000',
+    muscle: 'Traps',
+    name: 'Smith Machine Shrug'
+  }, {
+    equipment: 'Machine',
+    id: 'traps_001',
+    muscle: 'Traps',
+    name: 'Leverage Shrug'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'traps_002',
+    muscle: 'Traps',
+    name: 'Standing Dumbbell Upright Row'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'traps_003',
+    muscle: 'Traps',
+    name: 'Kettlebell Sumo High Pull'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'traps_004',
+    muscle: 'Traps',
+    name: 'Dumbbell Shrug'
+  }, {
+    equipment: 'Machine',
+    id: 'traps_005',
+    muscle: 'Traps',
+    name: 'Calf-Machine Shoulder Shrug'
+  }, {
+    equipment: 'Barbell',
+    id: 'traps_006',
+    muscle: 'Traps',
+    name: 'Barbell Shrug'
+  }, {
+    equipment: 'Barbell',
+    id: 'traps_007',
+    muscle: 'Traps',
+    name: 'Barbell Shrug Behind The Back'
+  }, {
+    equipment: 'Cable',
+    id: 'traps_008',
+    muscle: 'Traps',
+    name: 'Upright Cable Row'
+  }, {
+    equipment: 'Cable',
+    id: 'traps_009',
+    muscle: 'Traps',
+    name: 'Cable Shrugs'
+  }, {
+    equipment: 'Bands',
+    id: 'traps_010',
+    muscle: 'Traps',
+    name: 'Upright Row - With Bands'
+  }, {
+    equipment: 'Machine',
+    id: 'traps_011',
+    muscle: 'Traps',
+    name: 'Smith Machine Behind the Back Shrug'
+  }, {
+    equipment: 'Machine',
+    id: 'traps_012',
+    muscle: 'Traps',
+    name: 'Smith Machine Upright Row'
+  }, {
+    equipment: 'Barbell',
+    id: 'traps_013',
+    muscle: 'Traps',
+    name: 'Clean Shrug'
+  }, {
+    equipment: 'None',
+    id: 'traps_014',
+    muscle: 'Traps',
+    name: 'Scapular Pull-Up'
+  }, {
+    equipment: 'Barbell',
+    id: 'traps_015',
+    muscle: 'Traps',
+    name: 'Snatch Shrug'
+  }
+]
