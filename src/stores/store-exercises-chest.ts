9d13880e6c75d081f531a5ca5cdd5e7fc2ad6661
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:25:23 +0200

    Add library of chest exercises

---
+++ src/stores/store-exercises-chest.ts
@@ -0,0 +1,565 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesChestStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Dumbbell',
+    id: 'chest_000',
+    muscle: 'Chest',
+    name: 'Dumbbell Bench Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_001',
+    muscle: 'Chest',
+    name: 'Pushups'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_002',
+    muscle: 'Chest',
+    name: 'Dumbbell Flyes'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_003',
+    muscle: 'Chest',
+    name: 'Incline Dumbbell Press'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_004',
+    muscle: 'Chest',
+    name: 'Low Cable Crossover'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_005',
+    muscle: 'Chest',
+    name: 'Decline Dumbbell Flyes'
+  }, {
+    equipment: 'Other',
+    id: 'chest_006',
+    muscle: 'Chest',
+    name: 'Dips - Chest Version'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_007',
+    muscle: 'Chest',
+    name: 'Barbell Bench Press - Medium Grip'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'chest_008',
+    muscle: 'Chest',
+    name: 'Bodyweight Flyes'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_009',
+    muscle: 'Chest',
+    name: 'Incline Cable Flye'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_010',
+    muscle: 'Chest',
+    name: 'Wide-Grip Decline Barbell Bench Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_011',
+    muscle: 'Chest',
+    name: 'Incline Dumbbell Press Reverse-Grip'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_012',
+    muscle: 'Chest',
+    name: 'Wide-Grip Barbell Bench Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_013',
+    muscle: 'Chest',
+    name: 'Decline Barbell Bench Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_014',
+    muscle: 'Chest',
+    name: 'Incline Push-Up'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_015',
+    muscle: 'Chest',
+    name: 'Incline Dumbbell Flyes'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_016',
+    muscle: 'Chest',
+    name: 'Cable Crossover'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_017',
+    muscle: 'Chest',
+    name: 'Neck Press'
+  }, {
+    equipment: 'Other',
+    id: 'chest_018',
+    muscle: 'Chest',
+    name: 'Drop Push'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_019',
+    muscle: 'Chest',
+    name: 'Barbell Incline Bench Press Medium-Grip'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_020',
+    muscle: 'Chest',
+    name: 'Incline Push-Up Medium'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_021',
+    muscle: 'Chest',
+    name: 'Incline Cable Chest Press'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_022',
+    muscle: 'Chest',
+    name: 'Cable Chest Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_023',
+    muscle: 'Chest',
+    name: 'Dumbbell Bench Press with Neutral Grip'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_024',
+    muscle: 'Chest',
+    name: 'Leverage Incline Chest Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_025',
+    muscle: 'Chest',
+    name: 'Single-Arm Push-Up'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_026',
+    muscle: 'Chest',
+    name: 'Single-Arm Cable Crossover'
+  }, {
+    equipment: 'None',
+    id: 'chest_027',
+    muscle: 'Chest',
+    name: 'Decline Push-Up'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_028',
+    muscle: 'Chest',
+    name: 'Hammer Grip Incline DB Bench Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_029',
+    muscle: 'Chest',
+    name: 'Pushups (Close and Wide Hand Positions)'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_030',
+    muscle: 'Chest',
+    name: 'Push-Ups With Feet Elevated'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'chest_031',
+    muscle: 'Chest',
+    name: 'Push-Ups With Feet On An Exercise Ball'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_032',
+    muscle: 'Chest',
+    name: 'Decline Dumbbell Bench Press'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_033',
+    muscle: 'Chest',
+    name: 'Butterfly'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_034',
+    muscle: 'Chest',
+    name: 'One Arm Dumbbell Bench Press'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_035',
+    muscle: 'Chest',
+    name: 'Leverage Chest Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_036',
+    muscle: 'Chest',
+    name: 'Incline Push-Up Wide'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_037',
+    muscle: 'Chest',
+    name: 'Straight-Arm Dumbbell Pullover'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_038',
+    muscle: 'Chest',
+    name: 'Isometric Wipers'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_039',
+    muscle: 'Chest',
+    name: 'Incline Dumbbell Bench With Palms Facing In'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_040',
+    muscle: 'Chest',
+    name: 'Clock Push-Up'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_041',
+    muscle: 'Chest',
+    name: 'Decline Smith Press'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_042',
+    muscle: 'Chest',
+    name: 'Cable Iron Cross'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_043',
+    muscle: 'Chest',
+    name: 'Incline Dumbbell Flyes - With A Twist'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_044',
+    muscle: 'Chest',
+    name: 'Leverage Decline Chest Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_045',
+    muscle: 'Chest',
+    name: 'Push-Up Wide'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_046',
+    muscle: 'Chest',
+    name: 'Smith Machine Bench Press'
+  }, {
+    equipment: 'Bands',
+    id: 'chest_047',
+    muscle: 'Chest',
+    name: 'Cross Over - With Bands'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_048',
+    muscle: 'Chest',
+    name: 'Front Raise And Pullover'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'chest_049',
+    muscle: 'Chest',
+    name: 'Chest Push (multiple response)'
+  }, {
+    equipment: 'Other',
+    id: 'chest_050',
+    muscle: 'Chest',
+    name: 'Incline Push-Up Depth Jump'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_051',
+    muscle: 'Chest',
+    name: 'Bent-Arm Dumbbell Pullover'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_052',
+    muscle: 'Chest',
+    name: 'Standing Cable Chest Press'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_053',
+    muscle: 'Chest',
+    name: 'Machine Bench Press'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_054',
+    muscle: 'Chest',
+    name: 'Smith Machine Incline Bench Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'chest_055',
+    muscle: 'Chest',
+    name: 'Plyo Kettlebell Pushups'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_056',
+    muscle: 'Chest',
+    name: 'Close-Hands Push-Up'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_057',
+    muscle: 'Chest',
+    name: 'Around The Worlds'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_058',
+    muscle: 'Chest',
+    name: 'Tiger-Bend Push-Up'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_059',
+    muscle: 'Chest',
+    name: 'Barbell Bench Press-Wide Grip'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_060',
+    muscle: 'Chest',
+    name: 'Plyo Push-up'
+  }, {
+    equipment: 'Other',
+    id: 'chest_061',
+    muscle: 'Chest',
+    name: 'Suspended Push-Up'
+  }, {
+    equipment: 'Cable',
+    id: 'chest_062',
+    muscle: 'Chest',
+    name: 'Flat Bench Cable Flyes'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_063',
+    muscle: 'Chest',
+    name: 'Wide-Grip Decline Barbell Pullover'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_064',
+    muscle: 'Chest',
+    name: 'Smith Machine Decline Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_065',
+    muscle: 'Chest',
+    name: 'Isometric Chest Squeezes'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_066',
+    muscle: 'Chest',
+    name: 'Push Up to Side Plank'
+  }, {
+    equipment: 'Barbell',
+    id: 'chest_067',
+    muscle: 'Chest',
+    name: 'Barbell Guillotine Bench Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_068',
+    muscle: 'Chest',
+    name: 'Side To Side Push-Up'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'chest_069',
+    muscle: 'Chest',
+    name: 'One-Arm Flat Bench Dumbbell Flye'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'chest_070',
+    muscle: 'Chest',
+    name: 'Extended Range One-Arm Kettlebell Floor Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'chest_071',
+    muscle: 'Chest',
+    name: 'Alternating Floor Press'
+  }, {
+    equipment: 'Other',
+    id: 'chest_072',
+    muscle: 'Chest',
+    name: 'Forward Drag with Press'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'chest_073',
+    muscle: 'Chest',
+    name: 'Medicine Ball Chest Pass'
+  }, {
+    equipment: 'None',
+    id: 'chest_074',
+    muscle: 'Chest',
+    name: 'Dynamic Chest Stretch'
+  }, {
+    equipment: 'Other',
+    id: 'chest_075',
+    muscle: 'Chest',
+    name: 'Chain Press'
+  }, {
+    equipment: 'Bands',
+    id: 'chest_076',
+    muscle: 'Chest',
+    name: 'Bench Press - With Bands'
+  }, {
+    equipment: 'Other',
+    id: 'chest_077',
+    muscle: 'Chest',
+    name: 'Behind Head Chest Stretch'
+  }, {
+    equipment: 'None',
+    id: 'chest_078',
+    muscle: 'Chest',
+    name: 'Elbows Back'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'chest_079',
+    muscle: 'Chest',
+    name: 'Chest Stretch on Stability Ball'
+  }, {
+    equipment: 'Other',
+    id: 'chest_080',
+    muscle: 'Chest',
+    name: 'Chest And Front Of Shoulder Stretch'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'chest_081',
+    muscle: 'Chest',
+    name: 'One-Arm Kettlebell Floor Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'chest_082',
+    muscle: 'Chest',
+    name: 'Leg-Over Floor Press'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'chest_083',
+    muscle: 'Chest',
+    name: 'Chest Push (single response)'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_084',
+    muscle: 'Chest',
+    name: 'Incline Push-Up Reverse Grip'
+  }, {
+    equipment: 'Other',
+    id: 'chest_085',
+    muscle: 'Chest',
+    name: 'Suspended Chest Fly'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_086',
+    muscle: 'Chest',
+    name: 'Staggered Push-Up'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'chest_087',
+    muscle: 'Chest',
+    name: 'Medicine-Ball Push-Up'
+  }, {
+    equipment: 'Other',
+    id: 'chest_088',
+    muscle: 'Chest',
+    name: 'Suspended Push-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_089',
+    muscle: 'Chest',
+    name: 'Diamond Push-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_090',
+    muscle: 'Chest',
+    name: 'Typewriter Push-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_091',
+    muscle: 'Chest',
+    name: 'Clapping Push-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_092',
+    muscle: 'Chest',
+    name: 'Triple Clap Push-Up'
+  }, {
+    equipment: 'Other',
+    id: 'chest_093',
+    muscle: 'Chest',
+    name: 'Weighted Push-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_094',
+    muscle: 'Chest',
+    name: 'One-Arm Push-Up'
+  }, {
+    equipment: 'Machine',
+    id: 'chest_095',
+    muscle: 'Chest',
+    name: 'Bar Throw And Press'
+  }, {
+    equipment: 'Other',
+    id: 'chest_096',
+    muscle: 'Chest',
+    name: 'Decline Explosive Push-Up'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'chest_097',
+    muscle: 'Chest',
+    name: 'Chest Push from 3 point stance'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'chest_098',
+    muscle: 'Chest',
+    name: 'Chest Push with Run Release'
+  }, {
+    equipment: 'Other',
+    id: 'chest_099',
+    muscle: 'Chest',
+    name: 'Heavy Bag Thrust'
+  }, {
+    equipment: 'Bands',
+    id: 'chest_100',
+    muscle: 'Chest',
+    name: 'Bench Press With Short Bands'
+  }, {
+    equipment: 'Exercise Ball',
+    id: 'chest_101',
+    muscle: 'Chest',
+    name: 'Bosu Ball Push-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_102',
+    muscle: 'Chest',
+    name: 'Hand Release Push-Up'
+  }, {
+    equipment: 'Other',
+    id: 'chest_103',
+    muscle: 'Chest',
+    name: 'Svend Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_104',
+    muscle: 'Chest',
+    name: 'Wide-Hands Push-Up'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_105',
+    muscle: 'Chest',
+    name: 'Dive Bomber Push-Up'
+  }, {
+    equipment: 'Other',
+    id: 'chest_106',
+    muscle: 'Chest',
+    name: 'Weighted Push-Up'
+  }, {
+    equipment: 'Other',
+    id: 'chest_107',
+    muscle: 'Chest',
+    name: 'Bar Push-Up Smith Machine'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'chest_108',
+    muscle: 'Chest',
+    name: 'Burpee To Medicine Ball Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'chest_109',
+    muscle: 'Chest',
+    name: 'Superman Push-Up'
+  }, {
+    equipment: 'Bands',
+    id: 'chest_110',
+    muscle: 'Chest',
+    name: 'Feet-Elevated TRX Push-Up'
+  }
+]
