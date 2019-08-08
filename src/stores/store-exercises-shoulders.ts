5041d4fcd76e731689648679e824588b46ffec92
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:54:34 +0200

    Add library of shoulder exercises

---
+++ src/stores/store-exercises-shoulders.ts
@@ -0,0 +1,735 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesShouldersStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Barbell',
+    id: 'shoulders_000',
+    muscle: 'Shoulders',
+    name: 'Single-Arm Linear Jammer'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_001',
+    muscle: 'Shoulders',
+    name: 'Side Laterals to Front Raise'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_002',
+    muscle: 'Shoulders',
+    name: 'Standing Palm-In One-Arm Dumbbell Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_003',
+    muscle: 'Shoulders',
+    name: 'Clean and Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_004',
+    muscle: 'Shoulders',
+    name: 'Push Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_005',
+    muscle: 'Shoulders',
+    name: 'Clean and Jerk'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_006',
+    muscle: 'Shoulders',
+    name: 'Standing Palms-In Dumbbell Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_007',
+    muscle: 'Shoulders',
+    name: 'Standing Military Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_008',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Push Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_009',
+    muscle: 'Shoulders',
+    name: 'Seated Barbell Military Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_010',
+    muscle: 'Shoulders',
+    name: 'One-Arm Side Laterals'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_011',
+    muscle: 'Shoulders',
+    name: 'Power Partials'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_012',
+    muscle: 'Shoulders',
+    name: 'Seated Dumbbell Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_013',
+    muscle: 'Shoulders',
+    name: 'Standing Dumbbell Straight-Arm Front Delt Raise Above Head'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_014',
+    muscle: 'Shoulders',
+    name: 'Reverse Flyes'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_015',
+    muscle: 'Shoulders',
+    name: 'Standing Dumbbell Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_016',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Lying One-Arm Rear Lateral Raise'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_017',
+    muscle: 'Shoulders',
+    name: 'Front Dumbbell Raise'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_018',
+    muscle: 'Shoulders',
+    name: 'Alternating Deltoid Raise'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_019',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Shoulder Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_020',
+    muscle: 'Shoulders',
+    name: 'Single Dumbbell Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_021',
+    muscle: 'Shoulders',
+    name: 'Car Drivers'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_022',
+    muscle: 'Shoulders',
+    name: 'Standing Alternating Dumbbell Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_023',
+    muscle: 'Shoulders',
+    name: 'Snatch-Grip Behind-The-Neck Overhead Press'
+  }, {
+    equipment: 'Machine',
+    id: 'shoulders_024',
+    muscle: 'Shoulders',
+    name: 'Smith Machine Overhead Shoulder Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_025',
+    muscle: 'Shoulders',
+    name: 'Arnold Dumbbell Press'
+  }, {
+    equipment: 'Machine',
+    id: 'shoulders_026',
+    muscle: 'Shoulders',
+    name: 'Leverage Shoulder Press'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_027',
+    muscle: 'Shoulders',
+    name: 'Front Plate Raise'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_028',
+    muscle: 'Shoulders',
+    name: 'Dumbbell One-Arm Shoulder Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_029',
+    muscle: 'Shoulders',
+    name: 'Front Two-Dumbbell Raise'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_030',
+    muscle: 'Shoulders',
+    name: 'Front Cable Raise'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_031',
+    muscle: 'Shoulders',
+    name: 'Handstand Push-Ups'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_032',
+    muscle: 'Shoulders',
+    name: 'External Rotation with Cable'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_033',
+    muscle: 'Shoulders',
+    name: 'Front Incline Dumbbell Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_034',
+    muscle: 'Shoulders',
+    name: 'Barbell Shoulder Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_035',
+    muscle: 'Shoulders',
+    name: 'Two-Arm Kettlebell Military Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_036',
+    muscle: 'Shoulders',
+    name: 'Standing Bradford Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_037',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Rear Delt Row'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_038',
+    muscle: 'Shoulders',
+    name: 'Kettlebell Turkish Get-Up (Squat style)'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_039',
+    muscle: 'Shoulders',
+    name: 'Cable Internal Rotation'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_040',
+    muscle: 'Shoulders',
+    name: 'Side Lateral Raise'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_041',
+    muscle: 'Shoulders',
+    name: 'Alternating Kettlebell Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_042',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Snatch'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_043',
+    muscle: 'Shoulders',
+    name: 'Kettlebell Thruster'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_044',
+    muscle: 'Shoulders',
+    name: 'Battling Ropes'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_045',
+    muscle: 'Shoulders',
+    name: 'Bent Over Low-Pulley Side Lateral'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_046',
+    muscle: 'Shoulders',
+    name: 'Low Pulley Row To Neck'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_047',
+    muscle: 'Shoulders',
+    name: 'External Rotation'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_048',
+    muscle: 'Shoulders',
+    name: 'Barbell Rear Delt Row'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_049',
+    muscle: 'Shoulders',
+    name: 'Seated Cable Shoulder Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_050',
+    muscle: 'Shoulders',
+    name: 'One-Arm Incline Lateral Raise'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_051',
+    muscle: 'Shoulders',
+    name: 'Cable Rope Rear-Delt Rows'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_052',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Raise'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_053',
+    muscle: 'Shoulders',
+    name: 'Face Pull'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_054',
+    muscle: 'Shoulders',
+    name: 'Seated Bent-Over Rear Delt Raise'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_055',
+    muscle: 'Shoulders',
+    name: 'Side Wrist Pull'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_056',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Clean and Jerk'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_057',
+    muscle: 'Shoulders',
+    name: 'Standing Low-Pulley Deltoid Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_058',
+    muscle: 'Shoulders',
+    name: 'Upright Barbell Row'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_059',
+    muscle: 'Shoulders',
+    name: 'Seated Side Lateral Raise'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_060',
+    muscle: 'Shoulders',
+    name: 'Iron Cross'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_061',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Lying Rear Lateral Raise'
+  }, {
+    equipment: 'Bands',
+    id: 'shoulders_062',
+    muscle: 'Shoulders',
+    name: 'Internal Rotation with Band'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_063',
+    muscle: 'Shoulders',
+    name: 'Straight Raises on Incline Bench'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_064',
+    muscle: 'Shoulders',
+    name: 'Bent Over Dumbbell Rear Delt Raise With Head On Bench'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_065',
+    muscle: 'Shoulders',
+    name: 'Lying Rear Delt Raise'
+  }, {
+    equipment: 'Bands',
+    id: 'shoulders_066',
+    muscle: 'Shoulders',
+    name: 'Band Pull Apart'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_067',
+    muscle: 'Shoulders',
+    name: 'Bradford/Rocky Presses'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_068',
+    muscle: 'Shoulders',
+    name: 'Kettlebell Turkish Get-Up (Lunge style)'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_069',
+    muscle: 'Shoulders',
+    name: 'Cable Rear Delt Fly'
+  }, {
+    equipment: 'Machine',
+    id: 'shoulders_070',
+    muscle: 'Shoulders',
+    name: 'Reverse Machine Flyes'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_071',
+    muscle: 'Shoulders',
+    name: 'Anti-Gravity Press'
+  }, {
+    equipment: 'Bands',
+    id: 'shoulders_072',
+    muscle: 'Shoulders',
+    name: 'Back Flyes - With Bands'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_073',
+    muscle: 'Shoulders',
+    name: 'Cable Shoulder Press'
+  }, {
+    equipment: 'Machine',
+    id: 'shoulders_074',
+    muscle: 'Shoulders',
+    name: 'Machine Shoulder (Military) Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_075',
+    muscle: 'Shoulders',
+    name: 'Cuban Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_076',
+    muscle: 'Shoulders',
+    name: 'Reverse Flyes With External Rotation'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_077',
+    muscle: 'Shoulders',
+    name: 'Alternating Cable Shoulder Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_078',
+    muscle: 'Shoulders',
+    name: 'Lying One-Arm Lateral Raise'
+  }, {
+    equipment: 'Bands',
+    id: 'shoulders_079',
+    muscle: 'Shoulders',
+    name: 'Lateral Raise - With Bands'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_080',
+    muscle: 'Shoulders',
+    name: 'Standing Barbell Press Behind Neck'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_081',
+    muscle: 'Shoulders',
+    name: 'Kettlebell Pirate Ships'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_082',
+    muscle: 'Shoulders',
+    name: 'See-Saw Press (Alternating Side Press)'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_083',
+    muscle: 'Shoulders',
+    name: 'Dumbbell One-Arm Upright Row'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_084',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Scaption'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_085',
+    muscle: 'Shoulders',
+    name: 'Circus Bell'
+  }, {
+    equipment: 'Bands',
+    id: 'shoulders_086',
+    muscle: 'Shoulders',
+    name: 'Shoulder Press - With Bands'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_087',
+    muscle: 'Shoulders',
+    name: 'Two-Arm Kettlebell Clean'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_088',
+    muscle: 'Shoulders',
+    name: 'Double Kettlebell Push Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_089',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Incline Shoulder Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_090',
+    muscle: 'Shoulders',
+    name: 'Barbell Front Raise'
+  }, {
+    equipment: 'Machine',
+    id: 'shoulders_091',
+    muscle: 'Shoulders',
+    name: 'Smith Machine One-Arm Upright Row'
+  }, {
+    equipment: 'Machine',
+    id: 'shoulders_092',
+    muscle: 'Shoulders',
+    name: 'Machine Lateral Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_093',
+    muscle: 'Shoulders',
+    name: 'Standing Front Barbell Raise Over Head'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_094',
+    muscle: 'Shoulders',
+    name: 'Kettlebell Seated Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_095',
+    muscle: 'Shoulders',
+    name: 'Double Kettlebell Jerk'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'shoulders_096',
+    muscle: 'Shoulders',
+    name: 'Backward Medicine Ball Throw'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_097',
+    muscle: 'Shoulders',
+    name: 'Kettlebell Arnold Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_098',
+    muscle: 'Shoulders',
+    name: 'Landmine Linear Jammer'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_099',
+    muscle: 'Shoulders',
+    name: 'Upward Stretch'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_100',
+    muscle: 'Shoulders',
+    name: 'Shoulder Circles'
+  }, {
+    equipment: 'Cable',
+    id: 'shoulders_101',
+    muscle: 'Shoulders',
+    name: 'Cable Seated Lateral Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_102',
+    muscle: 'Shoulders',
+    name: 'Barbell Incline Shoulder Raise'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_103',
+    muscle: 'Shoulders',
+    name: 'Push Press - Behind the Neck'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_104',
+    muscle: 'Shoulders',
+    name: 'Crucifix'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_105',
+    muscle: 'Shoulders',
+    name: 'Shoulder Stretch'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_106',
+    muscle: 'Shoulders',
+    name: 'Round The World Shoulder Stretch'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_107',
+    muscle: 'Shoulders',
+    name: 'Elbow Circles'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_108',
+    muscle: 'Shoulders',
+    name: 'Arm Circles'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_109',
+    muscle: 'Shoulders',
+    name: 'Seated Front Deltoid'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_110',
+    muscle: 'Shoulders',
+    name: 'Shoulder Raise'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_111',
+    muscle: 'Shoulders',
+    name: 'Chair Upper Body Stretch'
+  }, {
+    equipment: 'None',
+    id: 'shoulders_112',
+    muscle: 'Shoulders',
+    name: 'Kneeling Arm Drill'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'shoulders_113',
+    muscle: 'Shoulders',
+    name: 'Medicine Ball Scoop Throw'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_114',
+    muscle: 'Shoulders',
+    name: 'Smith Incline Shoulder Raise'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_115',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Jerk'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_116',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Military Press To The Side'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_117',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Para Press'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_118',
+    muscle: 'Shoulders',
+    name: 'Two-Arm Kettlebell Jerk'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_119',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Split Jerk'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_120',
+    muscle: 'Shoulders',
+    name: 'Jerk Balance'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_121',
+    muscle: 'Shoulders',
+    name: 'Log Lift'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'shoulders_122',
+    muscle: 'Shoulders',
+    name: 'Return Push from Stance'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'shoulders_123',
+    muscle: 'Shoulders',
+    name: 'Standing Two-Arm Overhead Throw'
+  }, {
+    equipment: 'Bands',
+    id: 'shoulders_124',
+    muscle: 'Shoulders',
+    name: 'External Rotation with Band'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_125',
+    muscle: 'Shoulders',
+    name: 'Punches'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_126',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Squat To Shoulder Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_127',
+    muscle: 'Shoulders',
+    name: 'Hand Stand Push-Up'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_128',
+    muscle: 'Shoulders',
+    name: 'Front Dumbbell Raise Using Towel'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_129',
+    muscle: 'Shoulders',
+    name: 'Plate Shoulder Circle Big To Small Rotation'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_130',
+    muscle: 'Shoulders',
+    name: 'Bear Crawl Fire Feet'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_131',
+    muscle: 'Shoulders',
+    name: 'Wall Shoulder Tap'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_132',
+    muscle: 'Shoulders',
+    name: 'Bear Crawl Shoulder Tap'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_133',
+    muscle: 'Shoulders',
+    name: 'Double Kettlebell Snatch'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_134',
+    muscle: 'Shoulders',
+    name: 'One-Arm Kettlebell Split Snatch'
+  }, {
+    equipment: 'Kettlebells',
+    id: 'shoulders_135',
+    muscle: 'Shoulders',
+    name: 'Kettlebell Seesaw Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_136',
+    muscle: 'Shoulders',
+    name: 'Rack Delivery'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_137',
+    muscle: 'Shoulders',
+    name: 'Sled Overhead Backward Walk'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_138',
+    muscle: 'Shoulders',
+    name: 'Sled Reverse Flye'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_139',
+    muscle: 'Shoulders',
+    name: 'Axle Clean And Press'
+  }, {
+    equipment: 'Other',
+    id: 'shoulders_140',
+    muscle: 'Shoulders',
+    name: 'Suspended Back Fly'
+  }, {
+    equipment: 'Barbell',
+    id: 'shoulders_141',
+    muscle: 'Shoulders',
+    name: 'Tall Muscle Snatch'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_142',
+    muscle: 'Shoulders',
+    name: 'Half-kneeling Dumbbell Shoulder Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'shoulders_143',
+    muscle: 'Shoulders',
+    name: 'Wall Walk'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'shoulders_144',
+    muscle: 'Shoulders',
+    name: 'Dumbbell Clean And Jerk'
+  }
+]
