668388fef302ae052bb92c3b8532f874b8497336
Alex Devero <deveroalex@gmail.com>
2019-08-08 15:25:46 +0200

    Add library of triceps exercises

---
+++ src/stores/store-exercises-triceps.ts
@@ -0,0 +1,405 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesTricepsStore: {
+  equipment: string;
+  muscle: string;
+  id: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Body Only',
+    id: 'triceps_000',
+    muscle: 'Triceps',
+    name: 'Dips - Triceps Version'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_001',
+    muscle: 'Triceps',
+    name: 'Decline EZ Bar Triceps Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_002',
+    muscle: 'Triceps',
+    name: 'Dumbbell Floor Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_003',
+    muscle: 'Triceps',
+    name: 'Close-Grip Barbell Bench Press'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_004',
+    muscle: 'Triceps',
+    name: 'Triceps Pushdown - V-Bar Attachment'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_005',
+    muscle: 'Triceps',
+    name: 'Weighted Bench Dip'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_006',
+    muscle: 'Triceps',
+    name: 'Kneeling Cable Triceps Extension'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_007',
+    muscle: 'Triceps',
+    name: 'Reverse Grip Triceps Pushdown'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_008',
+    muscle: 'Triceps',
+    name: 'Standing Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_009',
+    muscle: 'Triceps',
+    name: 'Push-Ups - Close Triceps Position'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'triceps_010',
+    muscle: 'Triceps',
+    name: 'EZ-Bar Skullcrusher'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_011',
+    muscle: 'Triceps',
+    name: 'Triceps Pushdown - Rope Attachment'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_012',
+    muscle: 'Triceps',
+    name: 'Cable One Arm Tricep Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_013',
+    muscle: 'Triceps',
+    name: 'Decline Close-Grip Bench To Skull Crusher'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_014',
+    muscle: 'Triceps',
+    name: 'Seated Triceps Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_015',
+    muscle: 'Triceps',
+    name: 'Incline Barbell Triceps Extension'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'triceps_016',
+    muscle: 'Triceps',
+    name: 'Close-Grip EZ-Bar Press'
+  }, {
+    equipment: 'Machine',
+    id: 'triceps_017',
+    muscle: 'Triceps',
+    name: 'Dip Machine'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_018',
+    muscle: 'Triceps',
+    name: 'Tricep Dumbbell Kickback'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_019',
+    muscle: 'Triceps',
+    name: 'Standing Bent-Over Two-Arm Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_020',
+    muscle: 'Triceps',
+    name: 'Ring Dips'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_021',
+    muscle: 'Triceps',
+    name: 'Parallel Bar Dip'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_022',
+    muscle: 'Triceps',
+    name: 'Bench Dips'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_023',
+    muscle: 'Triceps',
+    name: 'Lying Close-Grip Barbell Triceps Extension Behind The Head'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_024',
+    muscle: 'Triceps',
+    name: 'Bench Press - Powerlifting'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_025',
+    muscle: 'Triceps',
+    name: 'Cable Lying Triceps Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_026',
+    muscle: 'Triceps',
+    name: 'Standing Overhead Barbell Triceps Extension'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_027',
+    muscle: 'Triceps',
+    name: 'Triceps Overhead Extension with Rope'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_028',
+    muscle: 'Triceps',
+    name: 'JM Press'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_029',
+    muscle: 'Triceps',
+    name: 'Triceps Pushdown'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_030',
+    muscle: 'Triceps',
+    name: 'Decline Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_031',
+    muscle: 'Triceps',
+    name: 'Dumbbell One-Arm Triceps Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_032',
+    muscle: 'Triceps',
+    name: 'Lying Dumbbell Tricep Extension'
+  }, {
+    equipment: 'Bands',
+    id: 'triceps_033',
+    muscle: 'Triceps',
+    name: 'Band Skull Crusher'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_034',
+    muscle: 'Triceps',
+    name: 'One Arm Pronated Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_035',
+    muscle: 'Triceps',
+    name: 'One Arm Supinated Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_036',
+    muscle: 'Triceps',
+    name: 'Reverse Triceps Bench Press'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_037',
+    muscle: 'Triceps',
+    name: 'Chain Handle Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_038',
+    muscle: 'Triceps',
+    name: 'Standing One-Arm Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_039',
+    muscle: 'Triceps',
+    name: 'Floor Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_040',
+    muscle: 'Triceps',
+    name: 'Bench Press with Chains'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_041',
+    muscle: 'Triceps',
+    name: 'Cobra Triceps Extension'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_042',
+    muscle: 'Triceps',
+    name: 'Body Tricep Press'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_043',
+    muscle: 'Triceps',
+    name: 'Low Cable Triceps Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_044',
+    muscle: 'Triceps',
+    name: 'Seated Bent-Over Two-Arm Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Medicine Ball',
+    id: 'triceps_045',
+    muscle: 'Triceps',
+    name: 'Supine Chest Throw'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_046',
+    muscle: 'Triceps',
+    name: 'Cable Rope Overhead Triceps Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_047',
+    muscle: 'Triceps',
+    name: 'Tate Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_048',
+    muscle: 'Triceps',
+    name: 'Close-Grip Dumbbell Press'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'triceps_049',
+    muscle: 'Triceps',
+    name: 'Lying Triceps Press'
+  }, {
+    equipment: 'E-Z Curl Bar',
+    id: 'triceps_050',
+    muscle: 'Triceps',
+    name: 'Lying Close-Grip Barbell Triceps Press To Chin'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_051',
+    muscle: 'Triceps',
+    name: 'Standing Low-Pulley One-Arm Triceps Extension'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_052',
+    muscle: 'Triceps',
+    name: 'Body-Up'
+  }, {
+    equipment: 'Machine',
+    id: 'triceps_053',
+    muscle: 'Triceps',
+    name: 'Smith Machine Close-Grip Bench Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_054',
+    muscle: 'Triceps',
+    name: 'Standing Bent-Over One-Arm Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_055',
+    muscle: 'Triceps',
+    name: 'Board Press'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_056',
+    muscle: 'Triceps',
+    name: 'Seated Bent-Over One-Arm Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Machine',
+    id: 'triceps_057',
+    muscle: 'Triceps',
+    name: 'Machine Triceps Extension'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_058',
+    muscle: 'Triceps',
+    name: 'Incline Push-Up Close-Grip'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_059',
+    muscle: 'Triceps',
+    name: 'One Arm Floor Press'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_060',
+    muscle: 'Triceps',
+    name: 'Close-Grip Push-Up off of a Dumbbell'
+  }, {
+    equipment: 'Cable',
+    id: 'triceps_061',
+    muscle: 'Triceps',
+    name: 'Cable Incline Triceps Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_062',
+    muscle: 'Triceps',
+    name: 'Pin Presses'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_063',
+    muscle: 'Triceps',
+    name: 'Standing Towel Triceps Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_064',
+    muscle: 'Triceps',
+    name: 'Dumbbell Tricep Extension - Pronated Grip'
+  }, {
+    equipment: 'None',
+    id: 'triceps_065',
+    muscle: 'Triceps',
+    name: 'Tricep Side Stretch'
+  }, {
+    equipment: 'None',
+    id: 'triceps_066',
+    muscle: 'Triceps',
+    name: 'Triceps Stretch'
+  }, {
+    equipment: 'Body Only',
+    id: 'triceps_067',
+    muscle: 'Triceps',
+    name: 'Overhead Triceps'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_068',
+    muscle: 'Triceps',
+    name: 'Floor Press with Chains'
+  }, {
+    equipment: 'Bands',
+    id: 'triceps_069',
+    muscle: 'Triceps',
+    name: 'Speed Band Overhead Triceps'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_070',
+    muscle: 'Triceps',
+    name: 'Sled Overhead Triceps Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_071',
+    muscle: 'Triceps',
+    name: 'JM Press'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_072',
+    muscle: 'Triceps',
+    name: 'JM Press With Bands'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_073',
+    muscle: 'Triceps',
+    name: 'Triceps Plank Extension'
+  }, {
+    equipment: 'Dumbbell',
+    id: 'triceps_074',
+    muscle: 'Triceps',
+    name: 'V-Sit Dumbbell Triceps Extension'
+  }, {
+    equipment: 'Barbell',
+    id: 'triceps_075',
+    muscle: 'Triceps',
+    name: 'Reverse Band Bench Press'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_076',
+    muscle: 'Triceps',
+    name: 'Suspended Triceps Press'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_077',
+    muscle: 'Triceps',
+    name: 'Body Triceps Press Using Flat Bench'
+  }, {
+    equipment: 'Other',
+    id: 'triceps_078',
+    muscle: 'Triceps',
+    name: 'Russian Bar Dip'
+  }
+]
