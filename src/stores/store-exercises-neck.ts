d14a12e03002240dcce9f686e9212a4921aed5ce
Alex Devero <deveroalex@gmail.com>
2019-08-08 16:54:25 +0200

    Add library of neck exercises

---
+++ src/stores/store-exercises-neck.ts
@@ -0,0 +1,55 @@
+// TODO: Implement firebase and get data from dtb
+
+export const ExercisesNeckStore: {
+  equipment: string;
+  id: string;
+  muscle: string;
+  name: string;
+}[] = [
+  {
+    equipment: 'Other',
+    id: 'neck_000',
+    muscle: 'Neck',
+    name: 'Lying Face Down Plate Neck Resistance'
+  }, {
+    equipment: 'Other',
+    id: 'neck_001',
+    muscle: 'Neck',
+    name: 'Lying Face Up Plate Neck Resistance'
+  }, {
+    equipment: 'Other',
+    id: 'neck_002',
+    muscle: 'Neck',
+    name: 'Seated Head Harness Neck Resistance'
+  }, {
+    equipment: 'Body Only',
+    id: 'neck_003',
+    muscle: 'Neck',
+    name: 'Isometric Neck Exercise - Sides'
+  }, {
+    equipment: 'Body Only',
+    id: 'neck_004',
+    muscle: 'Neck',
+    name: 'Isometric Neck Exercise - Front And Back'
+  }, {
+    equipment: 'Body Only',
+    id: 'neck_005',
+    muscle: 'Neck',
+    name: 'Neck Bridge Prone'
+  }, {
+    equipment: 'None',
+    id: 'neck_006',
+    muscle: 'Neck',
+    name: 'Side Neck Stretch'
+  }, {
+    equipment: 'None',
+    id: 'neck_007',
+    muscle: 'Neck',
+    name: 'Chin To Chest Stretch'
+  }, {
+    equipment: 'Other',
+    id: 'neck_008',
+    muscle: 'Neck',
+    name: 'Neck-SMR'
+  }
+]
