a054368dbedaf6df873d3ab0e8dcca0fcf2fea65
Alex Devero <deveroalex@gmail.com>
2019-08-12 14:36:05 +0200

    Add styles for Layout component for mobile

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+import { breakpoints, colors } from './constants'
+
 const PageLayout = styled.section`
   display: flex;
   flex: 1;
-  flex-flow: row nowrap;
+  flex-flow: column nowrap;
+  padding-top: 60px;
+  background-color: ${colors.gray};
+
+  @media (min-width: ${breakpoints.sm}) {
+    flex-flow: row nowrap;
+    padding-top: 0;
+  }
 `
 
 const Layout = ({ children }: any) => {
