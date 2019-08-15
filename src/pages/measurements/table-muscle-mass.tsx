80e60b80ea5dc4a7ef5b8b76d4e682cc3f6fbdcc
Alex Devero <deveroalex@gmail.com>
2019-08-15 16:30:39 +0200

    Add measurements table for muscle mass

---
+++ src/pages/measurements/table-muscle-mass.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import Table from 'react-bootstrap/Table'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+import { HeadingH6 } from './../../components/typography'
+
+export const TableMuscleMass = () => (
+  <>
+    <HeadingH6 style={{ marginBottom: 12 }}>Muscle mass %</HeadingH6>
+
+    <Table hover>
+      <thead style={{ fontSize: 13 }}>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>%</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>11.01.2019</td>
+          <td>58.4</td>
+          <td>
+            <Dropdown alignRight>
+              <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
+                More
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item>Edit</Dropdown.Item>
+                <Dropdown.Item>Delete</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </td>
+        </tr>
+
+        <tr>
+          <td>2</td>
+          <td>18.01.2019</td>
+          <td>58.5</td>
+          <td>
+            <Dropdown alignRight>
+              <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
+                More
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item>Edit</Dropdown.Item>
+                <Dropdown.Item>Delete</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  </>
+)
