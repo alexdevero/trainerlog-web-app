a84fa1777dc99f01f77fb176782c5744e0de1bc6
Alex Devero <deveroalex@gmail.com>
2019-08-20 17:10:41 +0200

    Add mock data for muscle mass

---
+++ src/data/data-muscle-mass.ts
@@ -0,0 +1,72 @@
+export const dataMuscleMass = [
+  {
+    data: 58.9,
+    id: 'dmcms_1',
+    key: new Date('2019-07-01T06:00:00.000Z')
+  },
+  {
+    data: 58.7,
+    id: 'dmcms_2',
+    key: new Date('2019-07-02T06:00:00.000Z')
+  },
+  {
+    data: 58.6,
+    id: 'dmcms_3',
+    key: new Date('2019-07-03T06:00:00.000Z')
+  },
+  {
+    data: 58.7,
+    id: 'dmcms_4',
+    key: new Date('2019-07-04T06:00:00.000Z')
+  },
+  {
+    data: 58.9,
+    id: 'dmcms_5',
+    key: new Date('2019-07-05T06:00:00.000Z')
+  },
+  {
+    data: 59.1,
+    id: 'dmcms_6',
+    key: new Date('2019-07-06T06:00:00.000Z')
+  },
+  {
+    data: 59.2,
+    id: 'dmcms_7',
+    key: new Date('2019-07-07T06:00:00.000Z')
+  },
+  {
+    data: 59.1,
+    id: 'dmcms_8',
+    key: new Date('2019-07-08T06:00:00.000Z')
+  },
+  {
+    data: 59.1,
+    id: 'dmcms_9',
+    key: new Date('2019-07-09T06:00:00.000Z')
+  },
+  {
+    data: 59.2,
+    id: 'dmcms_10',
+    key: new Date('2019-07-10T06:00:00.000Z')
+  },
+  {
+    data: 59.3,
+    id: 'dmcms_11',
+    key: new Date('2019-07-11T06:00:00.000Z')
+  },
+  {
+    data: 59.2,
+    id: 'dmcms_12',
+    key: new Date('2019-07-12T06:00:00.000Z')
+  },
+  {
+    data: 59.3,
+    id: 'dmcms_13',
+    key: new Date('2019-07-13T06:00:00.000Z')
+  },
+  {
+    data: 59.4,
+    id: 'dmcms_14',
+    key: new Date('2019-07-14T06:00:00.000Z')
+  }
+]
