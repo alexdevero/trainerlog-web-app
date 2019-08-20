d3f3e7b88524ecad6211fceab6e5f151df9155f6
Alex Devero <deveroalex@gmail.com>
2019-08-20 11:06:07 +0200

    Implement demo data in body fat table

--- src/pages/measurements/table-body-fat.tsx
+++ src/pages/measurements/table-body-fat.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import Table from 'react-bootstrap/Table'
 import Dropdown from 'react-bootstrap/Dropdown'
+import moment from 'moment'
 
 import { HeadingH6 } from './../../components/typography'
+
+import { dataBodyFat } from './../../data/data-body-fat'
 
 export const TableBodyFat = () => (
   <>
@@ -19,41 +22,27 @@ export const TableBodyFat = () => (
       </thead>
 
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>11.01.2019</td>
-          <td>7.7</td>
-          <td>
-            <Dropdown alignRight>
-              <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
-                More
-              </Dropdown.Toggle>
+        {dataBodyFat.map((item, index: number) => {
+          return (
+            <tr key={item.id}>
+              <td>{index + 1}</td>
+              <td>{moment(item.key).format('DD. MM. YYYY')}</td>
+              <td>{item.data}</td>
+              <td>
+                <Dropdown alignRight>
+                  <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
+                    More
+                  </Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                <Dropdown.Item>Edit</Dropdown.Item>
-                <Dropdown.Item>Delete</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </td>
-        </tr>
-
-        <tr>
-          <td>2</td>
-          <td>18.01.2019</td>
-          <td>7.8</td>
-          <td>
-            <Dropdown alignRight>
-              <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
-                More
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item>Edit</Dropdown.Item>
-                <Dropdown.Item>Delete</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </td>
-        </tr>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Edit</Dropdown.Item>
+                    <Dropdown.Item>Delete</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </td>
+            </tr>
+          )
+        })}
       </tbody>
     </Table>
   </>
