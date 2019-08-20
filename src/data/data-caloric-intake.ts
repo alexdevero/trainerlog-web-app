e3ae006497500279fa8223b0813420c373304b06
Alex Devero <deveroalex@gmail.com>
2019-08-20 17:10:30 +0200

    Add mock data for caloric intake

---
+++ src/data/data-caloric-intake.ts
@@ -0,0 +1,72 @@
+export const dataCaloricIntake = [
+  {
+    data: 2830,
+    id: 'dclin_1',
+    key: new Date('2019-07-01T06:00:00.000Z')
+  },
+  {
+    data: 2605,
+    id: 'dclin_2',
+    key: new Date('2019-07-02T06:00:00.000Z')
+  },
+  {
+    data: 2830,
+    id: 'dclin_3',
+    key: new Date('2019-07-03T06:00:00.000Z')
+  },
+  {
+    data: 2605,
+    id: 'dclin_4',
+    key: new Date('2019-07-04T06:00:00.000Z')
+  },
+  {
+    data: 2830,
+    id: 'dclin_5',
+    key: new Date('2019-07-05T06:00:00.000Z')
+  },
+  {
+    data: 2605,
+    id: 'dclin_6',
+    key: new Date('2019-07-06T06:00:00.000Z')
+  },
+  {
+    data: 2830,
+    id: 'dclin_7',
+    key: new Date('2019-07-07T06:00:00.000Z')
+  },
+  {
+    data: 2605,
+    id: 'dclin_8',
+    key: new Date('2019-07-08T06:00:00.000Z')
+  },
+  {
+    data: 2830,
+    id: 'dclin_9',
+    key: new Date('2019-07-09T06:00:00.000Z')
+  },
+  {
+    data: 2605,
+    id: 'dclin_10',
+    key: new Date('2019-07-10T06:00:00.000Z')
+  },
+  {
+    data: 2830,
+    id: 'dclin_11',
+    key: new Date('2019-07-11T06:00:00.000Z')
+  },
+  {
+    data: 2605,
+    id: 'dclin_12',
+    key: new Date('2019-07-12T06:00:00.000Z')
+  },
+  {
+    data: 2830,
+    id: 'dclin_13',
+    key: new Date('2019-07-13T06:00:00.000Z')
+  },
+  {
+    data: 2605,
+    id: 'dclin_14',
+    key: new Date('2019-07-14T06:00:00.000Z')
+  }
+]
