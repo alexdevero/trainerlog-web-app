fdd22b234ea1f9beb4e2b5d00b8e60d962fe4465
Alex Devero <deveroalex@gmail.com>
2019-08-20 17:10:49 +0200

    Add mock data for water intake

---
+++ src/data/data-water-intake.ts
@@ -0,0 +1,72 @@
+export const dataWaterIntake = [
+  {
+    data: 4.1,
+    id: 'dwtin_1',
+    key: new Date('2019-07-01T06:00:00.000Z')
+  },
+  {
+    data: 3.9,
+    id: 'dwtin_2',
+    key: new Date('2019-07-02T06:00:00.000Z')
+  },
+  {
+    data: 4.5,
+    id: 'dwtin_3',
+    key: new Date('2019-07-03T06:00:00.000Z')
+  },
+  {
+    data: 5.1,
+    id: 'dwtin_4',
+    key: new Date('2019-07-04T06:00:00.000Z')
+  },
+  {
+    data: 2.9,
+    id: 'dwtin_5',
+    key: new Date('2019-07-05T06:00:00.000Z')
+  },
+  {
+    data: 3.5,
+    id: 'dwtin_6',
+    key: new Date('2019-07-06T06:00:00.000Z')
+  },
+  {
+    data: 3.4,
+    id: 'dwtin_7',
+    key: new Date('2019-07-07T06:00:00.000Z')
+  },
+  {
+    data: 3.4,
+    id: 'dwtin_8',
+    key: new Date('2019-07-08T06:00:00.000Z')
+  },
+  {
+    data: 3.2,
+    id: 'dwtin_9',
+    key: new Date('2019-07-09T06:00:00.000Z')
+  },
+  {
+    data: 3.3,
+    id: 'dwtin_10',
+    key: new Date('2019-07-10T06:00:00.000Z')
+  },
+  {
+    data: 3.6,
+    id: 'dwtin_11',
+    key: new Date('2019-07-11T06:00:00.000Z')
+  },
+  {
+    data: 4.7,
+    id: 'dwtin_12',
+    key: new Date('2019-07-12T06:00:00.000Z')
+  },
+  {
+    data: 6.1,
+    id: 'dwtin_13',
+    key: new Date('2019-07-13T06:00:00.000Z')
+  },
+  {
+    data: 3.9,
+    id: 'dwtin_14',
+    key: new Date('2019-07-14T06:00:00.000Z')
+  }
+]
