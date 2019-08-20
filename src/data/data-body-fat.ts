95053dfcab45e3b179e29e4c4207e49c0e358d2f
Alex Devero <deveroalex@gmail.com>
2019-08-20 11:03:23 +0200

    Make ids for body fat an body weight unique

--- src/data/data-body-fat.ts
+++ src/data/data-body-fat.ts
@@ -1,72 +1,72 @@
 export const dataBodyFat = [
   {
-    key: new Date('2019-07-01T06:00:00.000Z'),
-    id: '1',
-    data: 8.7
+    data: 8.7,
+    id: 'dbdf_1',
+    key: new Date('2019-07-01T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-02T06:00:00.000Z'),
-    id: '2',
-    data: 9.0
+    data: 9.0,
+    id: 'dbdf_2',
+    key: new Date('2019-07-02T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-03T06:00:00.000Z'),
-    id: '3',
-    data: 9.5
+    data: 9.5,
+    id: 'dbdf_3',
+    key: new Date('2019-07-03T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-04T06:00:00.000Z'),
-    id: '4',
-    data: 9.8
+    data: 9.8,
+    id: 'dbdf_4',
+    key: new Date('2019-07-04T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-05T06:00:00.000Z'),
-    id: '5',
-    data: 9.5
+    data: 9.5,
+    id: 'dbdf_5',
+    key: new Date('2019-07-05T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-06T06:00:00.000Z'),
-    id: '6',
-    data: 9.4
+    data: 9.4,
+    id: 'dbdf_6',
+    key: new Date('2019-07-06T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-07T06:00:00.000Z'),
-    id: '7',
-    data: 9.4
+    data: 9.4,
+    id: 'dbdf_7',
+    key: new Date('2019-07-07T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-08T06:00:00.000Z'),
-    id: '8',
-    data: 9.4
+    data: 9.4,
+    id: 'dbdf_8',
+    key: new Date('2019-07-08T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-09T06:00:00.000Z'),
-    id: '9',
-    data: 9.1
+    data: 9.1,
+    id: 'dbdf_9',
+    key: new Date('2019-07-09T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-10T06:00:00.000Z'),
-    id: '10',
-    data: 9.2
+    data: 9.2,
+    id: 'dbdf_10',
+    key: new Date('2019-07-10T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-11T06:00:00.000Z'),
-    id: '11',
-    data: 9.0
+    data: 9.0,
+    id: 'dbdf_11',
+    key: new Date('2019-07-11T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-12T06:00:00.000Z'),
-    id: '12',
-    data: 9.3
+    data: 9.3,
+    id: 'dbdf_12',
+    key: new Date('2019-07-12T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-13T06:00:00.000Z'),
-    id: '13',
-    data: 9.6
+    data: 9.6,
+    id: 'dbdf_13',
+    key: new Date('2019-07-13T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-14T06:00:00.000Z'),
-    id: '14',
-    data: 9.4
+    data: 9.4,
+    id: 'dbdf_14',
+    key: new Date('2019-07-14T06:00:00.000Z')
   }
 ]
--- src/data/data-body-weight.ts
+++ src/data/data-body-weight.ts
@@ -1,72 +1,72 @@
 export const dataBodyWeight = [
   {
-    key: new Date('2019-07-01T06:00:00.000Z'),
-    id: '1',
-    data: 62.0
+    data: 62.0,
+    id: 'dbdw_1',
+    key: new Date('2019-07-01T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-02T06:00:00.000Z'),
-    id: '2',
-    data: 62.4
+    data: 62.4,
+    id: 'dbdw_2',
+    key: new Date('2019-07-02T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-03T06:00:00.000Z'),
-    id: '3',
-    data: 63.5
+    data: 63.5,
+    id: 'dbdw_3',
+    key: new Date('2019-07-03T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-04T06:00:00.000Z'),
-    id: '4',
-    data: 63.5
+    data: 63.5,
+    id: 'dbdw_4',
+    key: new Date('2019-07-04T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-05T06:00:00.000Z'),
-    id: '5',
-    data: 63.1
+    data: 63.1,
+    id: 'dbdw_5',
+    key: new Date('2019-07-05T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-06T06:00:00.000Z'),
-    id: '6',
-    data: 63.1
+    data: 63.1,
+    id: 'dbdw_6',
+    key: new Date('2019-07-06T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-07T06:00:00.000Z'),
-    id: '7',
-    data: 63.1
+    data: 63.1,
+    id: 'dbdw_7',
+    key: new Date('2019-07-07T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-08T06:00:00.000Z'),
-    id: '8',
-    data: 63.1
+    data: 63.1,
+    id: 'dbdw_8',
+    key: new Date('2019-07-08T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-09T06:00:00.000Z'),
-    id: '9',
-    data: 62.6
+    data: 62.6,
+    id: 'dbdw_9',
+    key: new Date('2019-07-09T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-10T06:00:00.000Z'),
-    id: '10',
-    data: 62.6
+    data: 62.6,
+    id: 'dbdw_10',
+    key: new Date('2019-07-10T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-11T06:00:00.000Z'),
-    id: '11',
-    data: 62.3
+    data: 62.3,
+    id: 'dbdw_11',
+    key: new Date('2019-07-11T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-12T06:00:00.000Z'),
-    id: '12',
-    data: 62.8
+    data: 62.8,
+    id: 'dbdw_12',
+    key: new Date('2019-07-12T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-13T06:00:00.000Z'),
-    id: '13',
-    data: 63.5
+    data: 63.5,
+    id: 'dbdw_13',
+    key: new Date('2019-07-13T06:00:00.000Z')
   },
   {
-    key: new Date('2019-07-14T06:00:00.000Z'),
-    id: '14',
-    data: 62.6
+    data: 62.6,
+    id: 'dbdw_14',
+    key: new Date('2019-07-14T06:00:00.000Z')
   }
 ]
