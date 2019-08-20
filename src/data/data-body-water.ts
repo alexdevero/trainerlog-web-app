9620bb3fa3f95b58ac9caff57aa5ee629c8956f8
Alex Devero <deveroalex@gmail.com>
2019-08-20 17:10:22 +0200

    Add mock data for body water

---
+++ src/data/data-body-water.ts
@@ -0,0 +1,72 @@
+export const dataBodyWater = [
+  {
+    data: 33.9,
+    id: 'dbdwt_1',
+    key: new Date('2019-07-01T06:00:00.000Z')
+  },
+  {
+    data: 33.7,
+    id: 'dbdwt_2',
+    key: new Date('2019-07-02T06:00:00.000Z')
+  },
+  {
+    data: 33.6,
+    id: 'dbdwt_3',
+    key: new Date('2019-07-03T06:00:00.000Z')
+  },
+  {
+    data: 33.7,
+    id: 'dbdwt_4',
+    key: new Date('2019-07-04T06:00:00.000Z')
+  },
+  {
+    data: 33.9,
+    id: 'dbdwt_5',
+    key: new Date('2019-07-05T06:00:00.000Z')
+  },
+  {
+    data: 33.8,
+    id: 'dbdwt_6',
+    key: new Date('2019-07-06T06:00:00.000Z')
+  },
+  {
+    data: 33.5,
+    id: 'dbdwt_7',
+    key: new Date('2019-07-07T06:00:00.000Z')
+  },
+  {
+    data: 33.4,
+    id: 'dbdwt_8',
+    key: new Date('2019-07-08T06:00:00.000Z')
+  },
+  {
+    data: 33.6,
+    id: 'dbdwt_9',
+    key: new Date('2019-07-09T06:00:00.000Z')
+  },
+  {
+    data: 33.4,
+    id: 'dbdwt_10',
+    key: new Date('2019-07-10T06:00:00.000Z')
+  },
+  {
+    data: 33.3,
+    id: 'dbdwt_11',
+    key: new Date('2019-07-11T06:00:00.000Z')
+  },
+  {
+    data: 33.1,
+    id: 'dbdwt_12',
+    key: new Date('2019-07-12T06:00:00.000Z')
+  },
+  {
+    data: 33.3,
+    id: 'dbdwt_13',
+    key: new Date('2019-07-13T06:00:00.000Z')
+  },
+  {
+    data: 33.4,
+    id: 'dbdwt_14',
+    key: new Date('2019-07-14T06:00:00.000Z')
+  }
+]
