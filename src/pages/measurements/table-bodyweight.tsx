7cd7c87a3bd819b3c9aea431c5eb4e877e57d24e
Alex Devero <deveroalex@gmail.com>
2019-08-20 11:06:14 +0200

    Implement demo data in body weight table

--- src/pages/measurements/table-bodyweight.tsx
+++ src/pages/measurements/table-bodyweight.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react'
 import Table from 'react-bootstrap/Table'
 import Dropdown from 'react-bootstrap/Dropdown'
+import moment from 'moment'
 
 import { SettingsStore } from './../../stores/store-settings'
 import { HeadingH6 } from './../../components/typography'
+
+import { dataBodyWeight } from './../../data/data-body-weight'
 
 export const TableBodyweight = () => (
   <>
@@ -20,41 +23,27 @@ export const TableBodyweight = () => (
       </thead>
 
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>11.01.2019</td>
-          <td>65.6</td>
-          <td>
-            <Dropdown alignRight>
-              <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
-                More
-              </Dropdown.Toggle>
+        {dataBodyWeight.map((item, index: number) => {
+          return (
+            <tr key={item.id}>
+              <td>{index + 1}</td>
+              <td>{moment(item.key).format('DD. MM. YYYY')}</td>
+              <td>{item.data}</td>
+              <td>
+                <Dropdown alignRight>
+                  <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
+                    More
+                  </Dropdown.Toggle>
 
-              <Dropdown.Menu>
-                <Dropdown.Item>Edit</Dropdown.Item>
-                <Dropdown.Item>Delete</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </td>
-        </tr>
-
-        <tr>
-          <td>2</td>
-          <td>18.01.2019</td>
-          <td>65.8</td>
-          <td>
-            <Dropdown alignRight>
-              <Dropdown.Toggle style={{ padding: 0, background: 'transparent', border: 'none' }} variant="light" id="dropdown-basic">
-                More
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item>Edit</Dropdown.Item>
-                <Dropdown.Item>Delete</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </td>
-        </tr>
+                  <Dropdown.Menu>
+                    <Dropdown.Item>Edit</Dropdown.Item>
+                    <Dropdown.Item>Delete</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </td>
+            </tr>
+          )
+        })}
       </tbody>
     </Table>
   </>
